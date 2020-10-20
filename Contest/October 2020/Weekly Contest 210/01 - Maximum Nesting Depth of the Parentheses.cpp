ac079ce8d1e28e9fc3183f29f4daf31de4546067
Akash Sahu <akkusahu559@gmail.com>
2020-10-20 14:00:34 +0530

    Wrong files deleted

---
+++ Contest/October 2020/Weekly Contest 210/01 - Maximum Nesting Depth of the Parentheses.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxDepth(string S) {        
+        int Max = 0, count = 0, itr;
+        for(itr = 0; itr < S.size(); itr++){
+            if(S[itr] == '(') count++;
+            else if(S[itr] == ')') count--;
+            Max = max(Max, count);
+        }
+        return Max;
+    }
+};
