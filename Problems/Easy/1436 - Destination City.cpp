19af9b0e7c6d5ec6b9f09e74cb3df6f8d5ac1790
Akash Sahu <akkusahu559@gmail.com>
2020-11-25 21:28:19 +0530

    Solution Added

---
+++ Problems/Easy/1436 - Destination City.cpp
@@ -0,0 +1,42 @@
+
+// Question) 1436. Destination City
+
+// Implementation
+// Time Complexity = O(N)
+
+
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {        
+        map<string, int> mp;
+        string str1, str2;
+        str1 = paths[0][0];
+        str2 = paths[0][1];
+        mp[str1] = 0;            
+        mp[str2] = 0;
+
+        for(int itr1 = 1 ; itr1 < paths.size(); itr1++){
+            str1 = paths[itr1][0];
+            str2 = paths[itr1][1];
+            mp[str1] = 0;
+            mp[str2] = 0;                
+        }
+        
+        for(int itr1 = 0 ; itr1 < paths.size(); itr1++){
+            str1 = paths[itr1][0];
+            str2 = paths[itr1][1];
+            mp[str1] = 1;
+            if(mp[str2] == 0) mp[str2] = 0;                
+        }
+
+        string res;
+
+        for(auto a : mp){
+            if(a.second == 0){         
+                res = a.first;
+                break;
+            } 
+        }
+        return res;
+    }
+};
