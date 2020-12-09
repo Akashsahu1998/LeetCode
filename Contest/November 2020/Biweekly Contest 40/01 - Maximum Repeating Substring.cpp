07ca9d3325a2ec64c68600f9f1efa71cb9a849b8
Akash Sahu <akkusahu559@gmail.com>
2020-12-09 21:41:51 +0530

    Updated Solution

--- Contest/November 2020/Biweekly Contest 40/01 - Maximum Repeating Substring.cpp
+++ Contest/November 2020/Biweekly Contest 40/01 - Maximum Repeating Substring.cpp
@@ -7,21 +7,15 @@
 
 class Solution {
 public:
-    int maxRepeating(string sequence, string word) {        
-        int sequenceLen = sequence.length(), wordLen = word.length(), res = 0; 
-        
-        for (int i = 0; i <= sequenceLen - wordLen; i++){  
-            int j; 
-            for (j = 0; j < wordLen; j++) {
-                if (sequence[i+j] != word[j])  break;                     
-            }                
-            
-            if (j == wordLen){ 
-               res++; 
-               j = 0; 
-            } 
-        } 
-        return res; 
+    int maxRepeating(string sequence, string word) {
+        int res = 0;
+        string newWord = word;
+        while(sequence.find(newWord) != string::npos)
+        {
+            res++;
+            newWord += word;
+        }
+        return res;
     }
 };
 
