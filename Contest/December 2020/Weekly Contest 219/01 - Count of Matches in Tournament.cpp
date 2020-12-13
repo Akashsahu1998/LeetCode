370011b3ee2f474a3ac7dbb90db55ca1c5958083
Akash Sahu <akkusahu559@gmail.com>
2020-12-13 16:54:29 +0530

    Another Solution Added in Java

--- Contest/December 2020/Weekly Contest 219/01 - Count of Matches in Tournament.cpp
+++ Contest/December 2020/Weekly Contest 219/01 - Count of Matches in Tournament.cpp
@@ -33,3 +33,23 @@ public:
 };
 
 
+// 3rd Approach in Java
+
+class Solution {
+    public int numberOfMatches(int n) {
+        int cnt = 0; 
+        while(n != 1){
+            if(n % 2 == 1){
+			    cnt += (n / 2);
+                n = (n / 2) + 1;
+            }
+            else{
+                cnt += n / 2;
+                n = n / 2;
+            }
+        }
+        return cnt;
+    }
+}
+
+
