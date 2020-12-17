07de754b81ef7a1023d69cb2e7faf3f2b1207ca3
Akash Sahu <akkusahu559@gmail.com>
2020-12-17 20:52:30 +0530

    Another solution added in Java

--- Problems/Easy/1351 - Count Negative Numbers in a Sorted Matrix.cpp
+++ Problems/Easy/1351 - Count Negative Numbers in a Sorted Matrix.cpp
@@ -18,3 +18,15 @@ int countNegatives(vector<vector<int>>& grid) {
 }
 
 
+// 2nd Approach in Java
+// Time Complexity = O(M*N), Space Complexity = O(1)
+
+public int countNegatives(int[][] grid) {
+    int countNegNum = 0;
+    for(int itr = 0; itr < grid.length; itr++){
+        for(int btr = 0; btr < grid[itr].length; btr++){
+            if(grid[itr][btr] < 0) countNegNum++;
+        }
+    }
+    return countNegNum;
+}
