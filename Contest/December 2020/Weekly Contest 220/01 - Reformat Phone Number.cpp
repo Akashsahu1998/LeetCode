1d0633f7d1a3bd925fdeae4fbe16fdac988273c7
Akash Sahu <akkusahu559@gmail.com>
2020-12-20 18:26:39 +0530

    Another solution added in Java

--- Contest/December 2020/Weekly Contest 220/01 - Reformat Phone Number.cpp
+++ Contest/December 2020/Weekly Contest 220/01 - Reformat Phone Number.cpp
@@ -85,3 +85,86 @@ string reformatNumber(string number) {
     }
     return ans;
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N), Space Complexity = O(N)
+
+public String reformatNumber(String number) {
+    String str = "";
+    for(int itr = 0; itr < number.length(); itr++){
+        if((number.charAt(itr) != ' ') && (number.charAt(itr) != '-')){
+            str = str + number.charAt(itr);
+        }
+    }
+    
+    String ans = "";
+    int len = str.length();
+    int rem = len % 3, div = len / 3;
+    
+    if(rem == 0){
+        int cnt = 0;
+        for(int itr = 0; itr < len; itr++){
+            
+            ans = ans + str.charAt(itr);
+            cnt++;    
+            
+            if(cnt == 3 && itr != len-1){
+                ans = ans + '-';
+                cnt = 0;
+            }
+        }
+    }
+    else if(rem == 1){
+        if (div != 0){
+            div--;
+            rem++;
+        }
+        int itr = 0, cnt = 0, n = div*3;
+        for( ; itr < n; itr++){
+            ans = ans + str.charAt(itr);
+            cnt++;    
+            
+            if(cnt == 3){
+                ans = ans + '-';
+                cnt = 0;
+            }
+        }
+        
+        cnt = 0;            
+        for(itr = n ; itr < len; itr++){
+            ans = ans + str.charAt(itr);
+            cnt++;    
+            
+            if(cnt == 2 && itr != len-1){
+                ans = ans + '-';
+                cnt = 0;
+            }
+        }            
+    }
+    else if(rem == 2){
+        rem--;
+        int itr = 0, cnt = 0, n = div*3;
+        for( ; itr < n; itr++){
+            ans = ans + str.charAt(itr);
+            cnt++;    
+            
+            if(cnt == 3){
+                ans = ans + '-';
+                cnt = 0;
+            }
+        }
+        
+        cnt = 0;            
+        for(itr = n ; itr < len; itr++){
+            ans = ans + str.charAt(itr);
+            cnt++;    
+            
+            if(cnt == 2 && itr != len-1){
+                ans = ans + '-';
+                cnt = 0;
+            }
+        }
+    }
+    return ans;
+}
