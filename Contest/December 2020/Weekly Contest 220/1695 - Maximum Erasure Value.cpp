13e4f2ffb2e9e53e14fba6b43a71639187493a5c
Akash Sahu <akkusahu559@gmail.com>
2020-12-20 18:11:05 +0530

    Another solution added in Java

--- Contest/December 2020/Weekly Contest 220/1695 - Maximum Erasure Value.cpp
+++ Contest/December 2020/Weekly Contest 220/1695 - Maximum Erasure Value.cpp
@@ -44,3 +44,49 @@ int maximumUniqueSubarray(vector<int>& nums) {
     }
     return sum;
 }
+
+
+// 3rd Approach in Java
+// Using Hash Map and Window Sliding Technique
+// Time Complexity = O(N), Space Complexity = O(N)
+
+public int maximumUniqueSubarray(int[] nums) {
+    int sum = 0, itr = 0, jtr = 0, localSum = 0;
+    HashMap<Integer, Integer> hmap = new HashMap<Integer, Integer>();
+    for( ; jtr < nums.length; jtr++){
+        while(hmap.containsKey(nums[jtr])){
+            hmap.remove(nums[itr]);                
+            localSum -= nums[itr];
+            itr++;
+        }
+        
+        if (hmap.containsKey(nums[jtr])) 
+            hmap.put(nums[jtr], hmap.get(nums[jtr]) + 1); 
+        else 
+            hmap.put(nums[jtr], 1);              
+        localSum += nums[jtr];
+        sum = Math.max(sum, localSum);
+    }
+    return sum;
+}
+
+
+// 4th Approach in Java
+// Using Hash Set and Window Sliding Technique
+// Time Complexity = O(N), Space Complexity = O(N)
+
+public int maximumUniqueSubarray(int[] nums) {
+    int sum = 0, itr = 0, jtr = 0, localSum = 0;
+    HashSet<Integer> hset = new HashSet<Integer>();
+    for( ; jtr < nums.length; jtr++){
+        while(hset.contains(nums[jtr])){
+            hset.remove(nums[itr]);                
+            localSum -= nums[itr];
+            itr++;
+        }
+        hset.add(nums[jtr]);
+        localSum += nums[jtr];
+        sum = Math.max(sum, localSum);
+    }
+    return sum;
+}
