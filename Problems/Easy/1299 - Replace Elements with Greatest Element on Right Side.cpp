d8447ef2e0a77a16ca124aef5df456aa06725fd8
Akash Sahu <akkusahu559@gmail.com>
2020-12-21 20:45:07 +0530

    Another solution added in Java

--- Problems/Easy/1299 - Replace Elements with Greatest Element on Right Side.cpp
+++ Problems/Easy/1299 - Replace Elements with Greatest Element on Right Side.cpp
@@ -56,3 +56,52 @@ vector<int> replaceElements(vector<int>& arr) {
     }
     return arr;
 }
+
+
+// 3rd Approach in Java
+// Time Complexity = O(N^2), Space Complexity = O(1)
+
+public int[] replaceElements(int[] arr) {
+    if(arr.length == 1) {
+        arr[0] = -1;
+        return arr;
+    }
+    int Max, itr, jtr, pos, flag = 0;
+    for(itr = 0; itr < arr.length; itr++){            
+        pos = itr;
+        Max = arr[itr];
+        for(jtr = itr+1; jtr < arr.length; jtr++){
+            if(arr[jtr] > Max){
+                Max = arr[jtr];
+                pos = jtr;
+            }                
+        }          
+        
+       if(flag == 0){
+           if(pos == itr+1){
+               flag = 1;
+               for(jtr = itr+1; jtr < arr.length; jtr++){
+                   arr[jtr-1] = arr[jtr];
+               }
+               arr[jtr-1] = -1;
+           }
+           else arr[itr] = Max;
+       }
+        else arr[itr] = Max;
+    }
+    return arr;
+}
+
+
+// 4th Approach in Java
+// Time Complexity = O(N), Space Complexity = O(1)
+
+public int[] replaceElements(int[] arr) {
+    int temp, Max = -1;
+    for(int itr = arr.length-1; itr > -1; itr--){
+        temp = arr[itr];
+        arr[itr] = Max;
+        Max = Math.max(Max, temp);
+    }
+    return arr;
+}
