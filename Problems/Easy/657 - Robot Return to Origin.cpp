c628f9e1c211c26d6e9b5de7230bcaceafb76640
Akash Sahu <akkusahu559@gmail.com>
2020-12-24 19:59:36 +0530

    Another solution added in Java

--- Problems/Easy/657 - Robot Return to Origin.cpp
+++ Problems/Easy/657 - Robot Return to Origin.cpp
@@ -33,3 +33,18 @@ bool judgeCircle(string moves) {
     }
     return (a == 0) && (b == 0);
 }
+
+
+// 3rd Approach in Java Wihtout Using Map
+// Time Complexity = O(N), Space Complexity = O(1)
+
+public boolean judgeCircle(String moves) {
+    int a = 0, b = 0;
+    for(int itr = 0; itr < moves.length(); itr++){
+        if(moves.charAt(itr) == 'U') a++;
+        if(moves.charAt(itr) == 'D') a--;
+        if(moves.charAt(itr) == 'R') b++;
+        if(moves.charAt(itr) == 'L') b--;            
+    }
+    return (a == 0) && (b == 0);
+}
