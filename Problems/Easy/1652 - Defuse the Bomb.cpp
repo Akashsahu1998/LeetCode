de6ece4dd1783262e224851474f8dbda12b9eea1
Akash Sahu <akkusahu559@gmail.com>
2020-12-25 22:18:26 +0530

    Another solution added in Java

--- Problems/Easy/1652 - Defuse the Bomb.cpp
+++ Problems/Easy/1652 - Defuse the Bomb.cpp
@@ -45,3 +45,48 @@ vector<int> decrypt(vector<int>& code, int k) {
     }
     return vect;
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N*K), Space Complexity = O(N)
+
+public int[] decrypt(int[] code, int k) {
+    int n = code.length;
+    if(k == 0){
+        for(int itr = 0; itr < n; itr++)
+            code[itr] = 0;
+        return code;            
+    }        
+    int[] arr = new int[n];
+    int index = 0;
+    if(k > 0){            
+        for(int itr = 0; itr < n; itr++){
+            int cnt = k, jtr, total = 0;                 
+            if(itr == n-1) jtr = 0;
+            else jtr = itr+1;                    
+            for( ; jtr < n; jtr++){
+                cnt--;
+                total += code[jtr];                    
+                if(cnt == 0) break;                    
+                if(jtr == n-1) jtr = -1;
+            }
+            arr[index++] = total;
+        }            
+    }
+    else{
+        k = k * -1;
+        for(int itr = 0; itr < n; itr++){
+            int cnt = k, jtr, total = 0;
+            if(itr == 0) jtr = n-1;
+            else jtr = itr-1;
+            for( ; jtr > -1; jtr--){
+                cnt--;
+                total += code[jtr];
+                if(cnt == 0) break;
+                if(jtr == 0) jtr = n;
+            }
+            arr[index++] = total;
+        }
+    }
+    return arr;
+}
