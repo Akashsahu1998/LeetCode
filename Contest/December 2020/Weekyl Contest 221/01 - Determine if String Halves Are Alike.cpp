585a7ca2407b918b5386c3c397ccd554d8a2340a
Akash Sahu <akkusahu559@gmail.com>
2020-12-27 20:54:12 +0530

    Another solution added in Java

--- Contest/December 2020/Weekyl Contest 221/01 - Determine if String Halves Are Alike.cpp
+++ Contest/December 2020/Weekyl Contest 221/01 - Determine if String Halves Are Alike.cpp
@@ -22,3 +22,21 @@ bool halvesAreAlike(string str) {
     return false;
 }
 
+
+// 2nd Approach in Java
+// Time Complexity = O(N), Space Complexity = O(1)
+
+public boolean halvesAreAlike(String str) {
+    int count1 = 0, count2 = 0;
+    str = str.toLowerCase(); 
+    for(int itr = 0; itr < (str.length()/2); itr++){
+        if((str.charAt(itr) == 'a') || (str.charAt(itr) == 'e') || (str.charAt(itr) == 'i') || (str.charAt(itr) == 'o') || (str.charAt(itr) == 'u'))
+            count1++;
+    }
+    for(int itr = (str.length()/2); itr < str.length(); itr++){
+        if((str.charAt(itr) == 'a') || (str.charAt(itr) == 'e') || (str.charAt(itr) == 'i') || (str.charAt(itr) == 'o') || (str.charAt(itr) == 'u'))
+            count2++;
+    }
+    if(count1 == count2) return true;
+    return false;
+}
