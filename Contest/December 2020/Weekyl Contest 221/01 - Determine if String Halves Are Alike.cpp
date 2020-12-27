0b4a4907b97148db46bb1150a2163fa5758bd9b3
Akash Sahu <akkusahu559@gmail.com>
2020-12-27 20:53:34 +0530

    Solution Added

---
+++ Contest/December 2020/Weekyl Contest 221/01 - Determine if String Halves Are Alike.cpp
@@ -0,0 +1,24 @@
+
+// Question) 1704. Determine if String Halves Are Alike
+
+
+// Implementation
+
+// 1st Approach in C++
+// Time Complexity = O(N), Space Complexity = O(1)
+
+bool halvesAreAlike(string str) {
+    int count1 = 0, count2 = 0;
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+    for(int itr = 0; itr < (str.size()/2); itr++){
+        if((str[itr] == 'a') || (str[itr] == 'e') || (str[itr] == 'i') || (str[itr] == 'o') || (str[itr] == 'u'))
+            count1++;
+    }
+    for(int itr = (str.size()/2); itr < str.size(); itr++){
+        if((str[itr] == 'a') || (str[itr] == 'e') || (str[itr] == 'i') || (str[itr] == 'o') || (str[itr] == 'u'))
+            count2++;
+    }
+    if(count1 == count2) return true;
+    return false;
+}
+
