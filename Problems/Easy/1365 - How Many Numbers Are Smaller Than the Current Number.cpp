18a6346cd13696897be355c63eeded47ff5112c2
Akash Sahu <akkusahu559@gmail.com>
2020-12-31 23:42:42 +0530

    Solution Added

---
+++ Problems/Easy/1365 - How Many Numbers Are Smaller Than the Current Number.cpp
@@ -0,0 +1,22 @@
+
+// Question) 1365. How Many Numbers Are Smaller Than the Current Number
+
+
+// Implementation
+
+// 1st Approach in C++
+// Time Complexity = O(N^2), Space Complexity = O(N)
+
+vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+    int size = nums.size();
+    vector<int> vect;
+    for(int itr = 0; itr < size; itr++){
+        int cnt = 0, temp = size-1, jtr = itr+1;
+        while(temp-- > 0){
+            if(jtr == size) jtr = 0;
+            if(nums[itr] > nums[jtr++]) cnt++;                                                    
+        }
+        vect.emplace_back(cnt);
+    }
+    return vect;
+}
