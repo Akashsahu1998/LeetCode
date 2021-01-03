9fd801acce711f09be290ddffb3ef01a5be3ba38
Akash Sahu <akkusahu559@gmail.com>
2021-01-03 23:55:48 +0530

    Another solution added in Java

--- Problems/Easy/1678 - Goal Parser Interpretation.cpp
+++ Problems/Easy/1678 - Goal Parser Interpretation.cpp
@@ -22,3 +22,23 @@ string interpret(string command) {
     }
     return res;
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N), Space Complexity = O(N)
+
+public String interpret(String command) {
+    String res = "";
+    for(int itr = 0; itr < command.length(); itr++){
+        if(command.charAt(itr) == 'G') res += 'G';
+        else if(command.charAt(itr) == '(' && command.charAt(itr+1) == ')'){
+            res += 'o';
+            itr++;
+        } 
+        else{
+             res += "al";
+            itr += 3;
+        }
+    }
+    return res;
+}
