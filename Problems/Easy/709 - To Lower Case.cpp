6ea1666ef868e8eed27f4d4dee8bb6b29ad86781
Akash Sahu <akkusahu559@gmail.com>
2021-01-07 23:37:43 +0530

    Another solution added in Java

--- Problems/Easy/709 - To Lower Case.cpp
+++ Problems/Easy/709 - To Lower Case.cpp
@@ -11,3 +11,11 @@ string toLowerCase(string str) {
     transform(str.begin(), str.end(), str.begin(), ::tolower); 
     return str;
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N), Space Complexity = O(1)
+
+public String toLowerCase(String str) {        
+    return str.toLowerCase();
+}
