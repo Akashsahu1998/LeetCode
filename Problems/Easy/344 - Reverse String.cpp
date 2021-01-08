2a847f2864ef3c851905c155f01eb6804cfc4338
Akash Sahu <akkusahu559@gmail.com>
2021-01-08 23:28:09 +0530

    Another solution added in Java

--- Problems/Easy/344 - Reverse String.cpp
+++ Problems/Easy/344 - Reverse String.cpp
@@ -16,3 +16,17 @@ void reverseString(vector<char>& s) {
         itr++; jtr--;
     }
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N), Space Complexity = O(1)
+
+public void reverseString(char[] s) {
+    int itr = 0, jtr = s.length-1;
+    while(itr <= jtr){
+        char ch = s[itr];
+        s[itr] = s[jtr];
+        s[jtr] = ch;
+        itr++; jtr--;
+    }        
+}
