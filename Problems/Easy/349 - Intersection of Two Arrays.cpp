3db4fb70a7fe0dea9efa65ac61d23a66ed6ca99a
Akash Sahu <akkusahu559@gmail.com>
2021-01-13 23:28:38 +0530

    Another solution added in Java

--- Problems/Easy/349 - Intersection of Two Arrays.cpp
+++ Problems/Easy/349 - Intersection of Two Arrays.cpp
@@ -25,3 +25,28 @@ vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
     }
     return vt;
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N*M), Space Complexity = O(N)
+// where N = nums1.length, M = nums2.length
+
+public int[] intersection(int[] nums1, int[] nums2) {
+    HashSet<Integer> set = new HashSet();  
+    
+    for(int itr = 0; itr < nums1.length; itr++){
+        for(int jtr = 0; jtr < nums2.length; jtr++){
+            if(nums1[itr] == nums2[jtr]){
+                set.add(nums1[itr]);
+                break;
+            }
+        }
+    }
+    
+    int[] arr = new int[set.size()];        
+    int jtr = 0;
+    for (int itr : set)  
+        arr[jtr++] = itr; 
+    
+    return arr;
+}
