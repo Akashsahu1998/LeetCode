a042a61c0d6ae509991f5511124bb3820d9a770e
Akash Sahu <akkusahu559@gmail.com>
2021-01-15 22:05:30 +0530

    Another solution added in Java

--- Problems/Easy/167 - Two Sum II - Input array is sorted.cpp
+++ Problems/Easy/167 - Two Sum II - Input array is sorted.cpp
@@ -19,3 +19,20 @@ vector<int> twoSum(vector<int>& numbers, int target) {
     ans.push_back(jtr+1);
     return ans;
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N), Space Complexity = O(1)
+
+public int[] twoSum(int[] numbers, int target) {
+    int[] ans = new int[2]; 
+    int itr = 0, jtr  = numbers.length-1;
+    while(itr <= jtr){
+        if(numbers[itr]+numbers[jtr] == target) break;
+        else if(numbers[itr]+numbers[jtr] > target) jtr--;
+        else itr++;         
+    }
+    ans[0] = itr+1;
+    ans[1] = jtr+1;       
+    return ans;
+}
