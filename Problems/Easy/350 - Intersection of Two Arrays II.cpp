f3458435ae61e238ae645ccb58a94d8aa2e3e066
Akash Sahu <akkusahu559@gmail.com>
2021-01-16 23:20:41 +0530

    Another solution added in Java with optimized approach

--- Problems/Easy/350 - Intersection of Two Arrays II.cpp
+++ Problems/Easy/350 - Intersection of Two Arrays II.cpp
@@ -40,3 +40,61 @@ vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
     }
     return res;       
 }
+
+
+// 3rd Approach in Java
+// Time Complexity = O(M*N), Space Complexity = O(M)
+
+public int[] intersect(int[] nums1, int[] nums2) {
+    ArrayList<Integer> arr = new ArrayList<Integer>();
+    for(int itr = 0; itr < nums1.length; itr++){
+        for(int jtr = 0; jtr < nums2.length; jtr++){
+            if(nums1[itr] == nums2[jtr]){
+                nums2[jtr] = -11;
+                arr.add(nums1[itr]);
+                break;
+            }
+        }   
+    }
+    int[] res = new int[arr.size()];
+    for(int itr = 0; itr < arr.size(); itr++){
+        res[itr] = arr.get(itr);
+    }
+    return res; 
+}
+
+
+// 4th Approach in Java
+// Time Complexity = O(M+N), Space Complexity = O(M)
+
+public int[] intersect(int[] nums1, int[] nums2) {
+    HashMap<Integer, Integer> temp = new HashMap<Integer, Integer>();
+    ArrayList<Integer> arr = new ArrayList<Integer>();
+    
+    for(int itr = 0; itr < nums1.length; itr++){
+        if (temp.containsKey(nums1[itr]))              { 
+            temp.put(nums1[itr], temp.get(nums1[itr]) + 1); 
+        }  
+        else{ 
+            temp.put(nums1[itr], 1); 
+        } 
+    }
+    
+    for(int itr = 0; itr < nums2.length; itr++){
+        if(temp.containsKey(nums2[itr])){
+            int n = temp.get(nums2[itr]) - 1;
+            if(n >= 0){
+                temp.put(nums2[itr], temp.get(nums2[itr]) - 1); 
+                 arr.add(nums2[itr]);
+            }
+        }
+    }
+    
+    int[] res = new int[arr.size()];
+    for(int itr = 0; itr < arr.size(); itr++){
+        res[itr] = arr.get(itr);
+    }
+    return res;       
+}
+
+
