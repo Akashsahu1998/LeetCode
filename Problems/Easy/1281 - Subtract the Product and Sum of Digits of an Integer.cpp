080441204352b6e9899019e3565f4331b6abcdd2
Akash Sahu <akkusahu559@gmail.com>
2021-01-18 21:39:51 +0530

    Another solution added in Java

--- Problems/Easy/1281 - Subtract the Product and Sum of Digits of an Integer.cpp
+++ Problems/Easy/1281 - Subtract the Product and Sum of Digits of an Integer.cpp
@@ -30,3 +30,31 @@ int subtractProductAndSum(int n) {
     int sod = sumOfDigit(n);
     return pod - sod;        
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(LogN), Space Complexity = O(1)
+
+int productOfDigit(int n){
+    int pod = 1;
+    while(n > 0){
+        pod *= n%10;
+        n /= 10;
+    }        
+    return pod;
+}
+
+int sumOfDigit(int n){
+    int sod = 0;
+    while(n > 0){
+        sod += n%10;
+        n /= 10;
+    }        
+    return sod;
+}
+
+public int subtractProductAndSum(int n) {
+    int pod = productOfDigit(n);
+    int sod = sumOfDigit(n);
+    return pod - sod;        
+}
