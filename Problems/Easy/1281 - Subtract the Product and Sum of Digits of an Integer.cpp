9469303726d384bb7aa4a884ce79f2c55c9389e8
Akash Sahu <akkusahu559@gmail.com>
2021-01-18 21:38:32 +0530

    Solution added

---
+++ Problems/Easy/1281 - Subtract the Product and Sum of Digits of an Integer.cpp
@@ -0,0 +1,32 @@
+
+// Question) 1281. Subtract the Product and Sum of Digits of an Integer
+
+
+// Implementation
+
+// 1st Approach in C++
+// Time Complexity = O(LogN), Space Complexity = O(1)
+
+int productOfDigit(int n){
+    int pod = 1;
+    while(n > 0){
+        pod *= n%10;
+        n /= 10;
+    }        
+    return pod;
+}
+
+int sumOfDigit(int n){
+    int sod = 0;
+    while(n > 0){
+        sod += n%10;
+        n /= 10;
+    }        
+    return sod;
+}
+
+int subtractProductAndSum(int n) {
+    int pod = productOfDigit(n);
+    int sod = sumOfDigit(n);
+    return pod - sod;        
+}
