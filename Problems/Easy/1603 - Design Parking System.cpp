41fa14eedbb56afc57434c3ce8c391db7649a0fa
Akash Sahu <akkusahu559@gmail.com>
2021-01-22 23:49:56 +0530

    Another solution added in Java

--- Problems/Easy/1603 - Design Parking System.cpp
+++ Problems/Easy/1603 - Design Parking System.cpp
@@ -22,3 +22,20 @@ public:
     }
 };
 
+
+// 2nd Approach in Java
+
+class ParkingSystem {
+    int[] parking;
+    public ParkingSystem(int big, int medium, int small) {
+        parking = new int[]{big, medium, small};
+    }
+    
+    public boolean addCar(int carType) {
+        if(parking[carType-1] != 0){
+          parking[carType-1] = parking[carType-1] - 1;
+            return true;  
+        } 
+        else return false;
+    }
+}
