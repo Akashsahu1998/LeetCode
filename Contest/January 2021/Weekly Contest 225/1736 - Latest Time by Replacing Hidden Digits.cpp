2ef02258cf0b4d3072da315c738969a3244e6db1
Akash Sahu <akkusahu559@gmail.com>
2021-01-25 23:39:20 +0530

    Another solution added in Java

--- Contest/January 2021/Weekly Contest 225/1736 - Latest Time by Replacing Hidden Digits.cpp
+++ Contest/January 2021/Weekly Contest 225/1736 - Latest Time by Replacing Hidden Digits.cpp
@@ -5,7 +5,7 @@
 // Implementation
 
 // 1st Approach in C++
-// Time Complexity = O(N), Space Complexity = O(1)
+// Time Complexity = O(1), Space Complexity = O(N)
 
 string maximumTime(string str) {
     string res = "";
@@ -35,5 +35,40 @@ string maximumTime(string str) {
         res += '9';
     }
     else res += str[4];
+    return res;
+}
+
+
+// 2nd Approach in Java
+// Time Complexity = O(1), Space Complexity = O(N)
+
+public String maximumTime(String str) {
+    String res = "";
+    if(str.charAt(0) == '?'){
+        if(str.charAt(1) > '3' && str.charAt(1) != '?'){
+            res += '1';
+        } 
+        else res += '2';            
+    }
+    else res += str.charAt(0);
+    
+    if(str.charAt(1) == '?'){
+        if(str.charAt(0) == '2') res += '3';    
+        else if(str.charAt(0) != '?') res += '9';
+        else res += '3';
+    }
+    else res += str.charAt(1);
+    
+    res += ':';
+    
+    if(str.charAt(3) == '?'){
+        res += '5';            
+    }
+    else res += str.charAt(3);
+    
+    if(str.charAt(4) == '?'){
+        res += '9';
+    }
+    else res += str.charAt(4);
     return res;
 }
