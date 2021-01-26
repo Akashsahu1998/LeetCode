8c7267876ab17130c2c765679f2c32cdcbc37273
Akash Sahu <akkusahu559@gmail.com>
2021-01-26 22:14:13 +0530

    Another solution added in Java

--- Problems/Easy/1431 - Kids With the Greatest Number of Candies.cpp
+++ Problems/Easy/1431 - Kids With the Greatest Number of Candies.cpp
@@ -18,3 +18,19 @@ vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
     }
     return result;
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N), Space Complexity = O(N)
+
+public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+    List<Boolean> result = new ArrayList<Boolean>();    
+    int maxCandies = Arrays.stream(candies).max().getAsInt();
+    for(int candy : candies){
+        if((candy+extraCandies) >= maxCandies){
+            result.add(true);
+        }
+        else result.add(false);
+    }
+    return result;
+}
