00dc64b01f2468bf8dab41f7609ed659a792f8e9
Akash Sahu <akkusahu559@gmail.com>
2021-01-29 23:48:14 +0530

    Another solution added in Java

--- Problems/Easy/1486 - XOR Operation in an Array.cpp
+++ Problems/Easy/1486 - XOR Operation in an Array.cpp
@@ -18,3 +18,20 @@ int xorOperation(int n, int start) {
     }
     return res;
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N), Space Complexity = O(N)
+
+public int xorOperation(int n, int start) {
+    int[] arr = new int[n];     
+    for(int itr = 0; itr < n; itr++){
+        arr[itr] = start+(2*itr);
+    }
+    int res = arr[0];
+    for(int itr = 1; itr < n; itr++){
+	    res = res ^ arr[itr];	
+    }
+    return res;
+}
+
