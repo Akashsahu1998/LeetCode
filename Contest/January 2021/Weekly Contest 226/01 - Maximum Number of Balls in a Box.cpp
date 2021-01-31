a80831f918dae89a116c032f59a445e372412a71
Akash Sahu <akkusahu559@gmail.com>
2021-01-31 23:47:28 +0530

    Another Solution added in C++

--- Contest/January 2021/Weekly Contest 226/01 - Maximum Number of Balls in a Box.cpp
+++ Contest/January 2021/Weekly Contest 226/01 - Maximum Number of Balls in a Box.cpp
@@ -20,3 +20,21 @@ int countBalls(int lowLimit, int highLimit){
     }
     return max;
 }
+
+
+// 2nd Approach in C++
+// Time Complexity = O(NLogN), Space Complexity = O(N) where N = 1024
+
+int countBalls(int lowLimit, int highLimit) {        
+    unordered_map<int, int> ump;
+    int Max = -1;
+    for(int itr = lowLimit; itr <= highLimit; itr++){
+        int digitOfSum = 0;
+        for(int jtr = itr; jtr > 0; jtr /= 10){
+            digitOfSum += jtr%10;
+        }
+        ump[digitOfSum]++;
+        Max = max(Max, ump[digitOfSum]);                                    
+    }
+    return Max;
+}
