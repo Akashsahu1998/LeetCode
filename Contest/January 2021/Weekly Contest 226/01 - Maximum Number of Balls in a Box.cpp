e127498a14079c7936bb782fea3bb6ed16d97f98
Akash Sahu <akkusahu559@gmail.com>
2021-01-31 23:46:14 +0530

    Solution added in C

---
+++ Contest/January 2021/Weekly Contest 226/01 - Maximum Number of Balls in a Box.cpp
@@ -0,0 +1,22 @@
+
+// Question) 1742. Maximum Number of Balls in a Box
+
+
+// Implementation
+
+// 1st Approach in C
+// Time Complexity = O(NLogN), Space Complexity = O(N) where N = 1024
+
+int countBalls(int lowLimit, int highLimit){
+    int array[1024] = {0};
+    int max = -1;
+    for (int itr = lowLimit; itr <= highLimit; itr++) {
+        int sumOfDigit = 0;
+        for (int jtr = itr; jtr > 0; jtr /= 10) {
+            sumOfDigit += jtr % 10;
+        }
+        ++array[sumOfDigit];
+        if(max < array[sumOfDigit]) max = array[sumOfDigit];        
+    }
+    return max;
+}
