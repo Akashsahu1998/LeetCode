212b98dfef924f4cf6f20ebd99d88c00af7e3f6c
Akash Sahu <akkusahu559@gmail.com>
2021-02-04 23:26:00 +0530

    2nd Approach added into Java

--- Problems/Easy/1002 - Find Common Characters.cpp
+++ Problems/Easy/1002 - Find Common Characters.cpp
@@ -42,3 +42,40 @@ vector<string> commonChars(vector<string>& A) {
 }
 
 
+// 2nd Approach in Java
+// Time Complexity = O(N^2), Space Complexity = O(N)
+
+public List<String> commonChars(String[] A) {
+    int[] check1 = new int[26];
+    int[] check2 = new int[26];
+    List<String> result = new ArrayList<String>();
+    
+    String str = A[0];
+    for(int itr = 0; itr < str.length(); itr++){
+        check1[str.charAt(itr)-'a']++;
+    }
+    
+    for(int itr = 1; itr < A.length; itr++){
+        str = A[itr];
+        for(int jtr = 0; jtr < str.length(); jtr++){
+            check2[str.charAt(jtr)-'a']++;
+        }            
+        
+        for(int jtr = 0; jtr < 26; jtr++){
+            check1[jtr] = Math.min(check1[jtr], check2[jtr]); 
+            check2[jtr] = 0;
+        }
+    }
+           
+    for(int itr = 0; itr < 26; itr++){
+        if(check1[itr] > 0){
+            int value = check1[itr];
+            while(value-- > 0){
+                char ch = (char)(itr + 'a');                                        
+                result.add(Character.toString((char)ch));                                
+            }                
+        }
+    }
+    return result;
+}
+
