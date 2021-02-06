14bda09e2f8722d2b91131341fb7b6e623b547b9
Akash Sahu <akkusahu559@gmail.com>
2021-02-06 23:50:18 +0530

    2nd Approach added into Java

--- Problems/Easy/1051 - Height Checker.cpp
+++ Problems/Easy/1051 - Height Checker.cpp
@@ -16,3 +16,21 @@ int heightChecker(vector<int>& heights) {
     }
     return count;
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N^2), Space Complexity = O(N)
+
+public int heightChecker(int[] heights) {
+    int backup[] = new int[heights.length];
+    for (int i = 0; i < heights.length; i++)
+        backup[i] = heights[i];
+    
+    Arrays.sort(heights);
+    
+    int count = 0;
+    for(int itr = 0; itr < heights.length; itr++){
+        if(heights[itr] != backup[itr]) count++;
+    }
+    return count;
+}
