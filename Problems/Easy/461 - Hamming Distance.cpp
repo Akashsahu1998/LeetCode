72cd4eb0032c8eba645e221083bbc55eb0f28abc
Akash Sahu <akkusahu559@gmail.com>
2021-02-10 22:04:57 +0530

    2nd Approach added into Java

--- Problems/Easy/461 - Hamming Distance.cpp
+++ Problems/Easy/461 - Hamming Distance.cpp
@@ -22,3 +22,27 @@ int hammingDistance(int x, int y) {
     }
     return cnt;
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N), Space Complexity = O(N)
+
+public int hammingDistance(int x, int y) {
+    ArrayList<Integer> v1 = new ArrayList<Integer>();
+    ArrayList<Integer> v2 = new ArrayList<Integer>();
+    while(x != 0 || y != 0){
+        v1.add(x%2);
+        v2.add(y%2);
+        x /= 2;
+        y /= 2;
+    }
+    Iterator itr1 = v1.iterator();
+    Iterator itr2 = v2.iterator(); 
+    int cnt = 0;
+    
+    while(itr1.hasNext() && itr2.hasNext()){
+        if(itr1.next() != itr2.next()) cnt++;
+    }
+    return cnt;
+}
+
