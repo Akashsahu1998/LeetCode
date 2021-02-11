64634ae24b1dc0b1dff30b7f1507883e611c85ed
Akash Sahu <akkusahu559@gmail.com>
2021-02-11 22:43:17 +0530

    2nd Approach added into Java

--- Problems/Easy/561 - Array Partition I.cpp
+++ Problems/Easy/561 - Array Partition I.cpp
@@ -15,3 +15,16 @@ int arrayPairSum(vector<int>& nums) {
     }
     return result;
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(NLogN), Space Complexity = O(1)
+
+public int arrayPairSum(int[] nums) {
+    Arrays.sort(nums);
+    int result = 0;
+    for(int itr = 0; itr < nums.length; itr += 2){
+        result += nums[itr];
+    }
+    return result;
+}
