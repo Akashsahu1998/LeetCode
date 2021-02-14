1ef636c8089e70d0a2d808ad56bc054b3edbab5d
Akash Sahu <akkusahu559@gmail.com>
2021-02-14 22:52:39 +0530

    2nd Approach added into Java

--- Problems/Easy/852 - Peak Index in a Mountain Array.cpp
+++ Problems/Easy/852 - Peak Index in a Mountain Array.cpp
@@ -10,3 +10,18 @@
 int peakIndexInMountainArray(vector<int>& arr) {
     return distance(arr.begin(), max_element(arr.begin(), arr.end()));
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N), Space Complexity = O(1)
+
+public int peakIndexInMountainArray(int[] arr) {
+    int highestIndex = 0, maxElement = arr[0];
+    for(int itr = 0; itr < arr.length; itr++){
+        if(maxElement < arr[itr]){
+            maxElement = arr[itr];
+            highestIndex = itr;
+        }
+    }
+    return highestIndex;
+}
