001f02079b2486f15f0228e1c3f0f2b378f419fd
Akash Sahu <akkusahu559@gmail.com>
2021-02-18 23:20:47 +0530

    2nd Approach added into Java

--- Problems/Easy/1502 - Can Make Arithmetic Progression From Sequence.cpp
+++ Problems/Easy/1502 - Can Make Arithmetic Progression From Sequence.cpp
@@ -15,3 +15,17 @@ bool canMakeArithmeticProgression(vector<int>& arr) {
     }
     return true;
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(NlogN), Space Complexity = O(1)
+
+public boolean canMakeArithmeticProgression(int[] arr) {
+    Arrays.sort(arr);
+    int difference = arr[1]-arr[0];
+    for(int itr = 2; itr < arr.length; itr++){
+        if(arr[itr]-arr[itr-1] != difference) return false;
+    }
+    return true;
+}
+
