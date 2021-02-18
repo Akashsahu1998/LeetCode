a784f00b1fc9d83e1384ab61f310af0dbfbc0e7d
Akash Sahu <akkusahu559@gmail.com>
2021-02-18 23:19:52 +0530

    1st Approach added into c++

---
+++ Problems/Easy/1502 - Can Make Arithmetic Progression From Sequence.cpp
@@ -0,0 +1,17 @@
+
+// Question) 1502. Can Make Arithmetic Progression From Sequence
+
+
+// Implementation
+
+// 1st Approach in C++
+// Time Complexity = O(NlogN), Space Complexity = O(1)
+
+bool canMakeArithmeticProgression(vector<int>& arr) {
+    sort(arr.begin(), arr.end());
+    int difference = arr[1]-arr[0];
+    for(int itr = 2; itr < arr.size(); itr++){
+        if(arr[itr]-arr[itr-1] != difference) return false;
+    }
+    return true;
+}
