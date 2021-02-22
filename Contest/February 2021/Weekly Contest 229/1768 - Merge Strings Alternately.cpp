3d118d1b4ebe5a48efd66c2155a7c7503de50204
Akash Sahu <akkusahu559@gmail.com>
2021-02-22 23:47:39 +0530

    2nd Approach added into Java

--- Contest/February 2021/Weekly Contest 229/1768 - Merge Strings Alternately.cpp
+++ Contest/February 2021/Weekly Contest 229/1768 - Merge Strings Alternately.cpp
@@ -27,3 +27,28 @@ string mergeAlternately(string word1, string word2) {
     }
     return result;
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N), Space Complexity = O(N)
+
+public String mergeAlternately(String word1, String word2) {
+    String result = "";
+    int itr = 0, jtr = 0, ktr = 0;
+    
+    while( (itr < word1.length()) || (jtr < word2.length())){
+        if(itr < word1.length()){
+            if(ktr % 2 == 0){
+                result += word1.charAt(itr++);
+            }                
+        }
+        
+        if(jtr < word2.length()){
+            if(ktr % 2 == 1){
+                result += word2.charAt(jtr++);
+            }
+        }
+        ktr++;
+    }
+    return result;
+}
