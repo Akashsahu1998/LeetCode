f811ed2c6a7050b2a71f83dd2e55b941ebedf768
Akash Sahu <akkusahu559@gmail.com>
2021-02-24 23:23:41 +0530

    2nd Approach added into Java

--- Problems/Easy/1207 - Unique Number of Occurrences.cpp
+++ Problems/Easy/1207 - Unique Number of Occurrences.cpp
@@ -5,7 +5,7 @@
 // Implementation
 
 // 1st Approach in C++
-// Time Complexity = O(N), Space Complexity = O(1)
+// Time Complexity = O(N), Space Complexity = O(N)
 
 bool uniqueOccurrences(vector<int>& arr) {
     unordered_map<int, int> ump;
@@ -20,3 +20,28 @@ bool uniqueOccurrences(vector<int>& arr) {
     }            
     return true;        
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N), Space Complexity = O(1)
+
+public boolean uniqueOccurrences(int[] arr) {
+    HashMap<Integer, Integer> h1 = new HashMap<Integer, Integer>();
+    HashMap<Integer, Integer> h2 = new HashMap<Integer, Integer>();
+    
+    for(int a : arr){
+        if(h1.containsKey(a)) h1.put(a, h1.get(a)+1);
+        else h1.put(a, 1);            
+    } 
+    
+    for(Map.Entry<Integer, Integer> entry : h1.entrySet()){
+        if(h2.containsKey(entry.getValue())) h2.put(entry.getValue(), h2.get(entry.getValue())+1);
+        else h2.put(entry.getValue(), 1);            
+    }
+    
+    for(Map.Entry<Integer, Integer> entry : h2.entrySet()){
+        if(entry.getValue() > 1) return false;            
+    }
+    return true;
+}
+
