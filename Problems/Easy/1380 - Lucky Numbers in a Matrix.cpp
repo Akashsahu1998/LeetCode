372b993802c2f6dcca08ebe90d20457b39e6d1f4
Akash Sahu <akkusahu559@gmail.com>
2021-02-28 00:43:14 +0530

    2nd Approach added into Java

--- Problems/Easy/1380 - Lucky Numbers in a Matrix.cpp
+++ Problems/Easy/1380 - Lucky Numbers in a Matrix.cpp
@@ -30,3 +30,30 @@ vector<int> luckyNumbers (vector<vector<int>>& matrix) {
     }
     return vect;
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(M*N), Space Complexity = O(1)																		
+
+public List<Integer> luckyNumbers (int[][] matrix) {
+    int small, big;
+    List<Integer> list = new ArrayList<Integer>();
+    for(int itr = 0; itr < matrix.length; itr++){
+        int ktr = 0;
+        small = 999999;             
+        for(int jtr = 0; jtr < matrix[0].length; jtr++){                
+            if(small > matrix[itr][jtr]){
+                small = matrix[itr][jtr];
+                ktr = jtr;
+            }
+        }
+        big = small;
+        for(int jtr = 0; jtr < matrix.length; jtr++){
+            big = Math.max(big, matrix[jtr][ktr]);
+        }
+        if(small == big) {
+            list.add(small);
+        }
+    }
+    return list;
+}
