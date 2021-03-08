884709f42b425e936222971c61ff417520be9ed2
Akash Sahu <akash.sahu2@mindtree.com>
2021-03-08 23:53:58 +0530

    2nd Approach added into Java

--- Contest/March 2021/Weekly Contest 231/01. Check if Binary String Has at Most One Segment of Ones.cpp
+++ Contest/March 2021/Weekly Contest 231/01. Check if Binary String Has at Most One Segment of Ones.cpp
@@ -15,3 +15,18 @@ bool checkOnesSegment(string s) {
     }        
     return true;
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N), Space Complexity = O(1)
+
+public boolean checkOnesSegment(String s) {
+    if(s.length() == 1 && s.charAt(0) == '1') return true;
+    
+    for(int itr = 1; itr < s.length(); itr++){
+        if(s.charAt(itr-1) == '0' && s.charAt(itr) == '1') return false;
+    }        
+    return true;
+}
+
+
