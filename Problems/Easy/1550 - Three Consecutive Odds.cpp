a5161faad93b9388e8386c1bb2ff07f7657683df
Akash Sahu <akkusahu559@gmail.com>
2021-03-12 22:37:54 +0530

    Solution added into C++

---
+++ Problems/Easy/1550 - Three Consecutive Odds.cpp
@@ -0,0 +1,16 @@
+
+// Question) 1550. Three Consecutive Odds
+
+
+// Implementation
+
+// 1st Approach in C++
+// Time Complexity = O(N), Space Complexity = O(1)
+
+bool threeConsecutiveOdds(vector<int>& arr) {
+    if(arr.size() < 3) return false;
+    for(int itr = 0; itr < arr.size()-2; itr++){
+        if((arr[itr] % 2 == 1) && (arr[itr+1] % 2 == 1 && (arr[itr+2] % 2 == 1))) return true;
+    }
+    return false;
+}
