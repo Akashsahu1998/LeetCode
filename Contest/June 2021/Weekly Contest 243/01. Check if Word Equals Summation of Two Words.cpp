049f0d86f8318f3313943753c3007e348ad16dbb
Akash Sahu <akkusahu559@gmail.com>
2021-06-08 10:54:35 +0530

    Java Solution Added

--- Contest/June 2021/Weekly Contest 243/01. Check if Word Equals Summation of Two Words.cpp
+++ Contest/June 2021/Weekly Contest 243/01. Check if Word Equals Summation of Two Words.cpp
@@ -17,3 +17,18 @@ bool isSumEqual(string firstWord, string secondWord, string targetWord) {
     if((sumOfWord(firstWord) + sumOfWord(secondWord)) == sumOfWord(targetWord)) return true;
     else return false;
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N), Space Complexity = O(1)
+
+int sumOfWord(String str){
+    int sum = 0, itr = str.length(), jtr = 0;        
+    while(jtr < itr) sum = sum * 10 + ((str.charAt(jtr++)) - 97);
+    return sum;
+}
+
+public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
+    if((sumOfWord(firstWord) + sumOfWord(secondWord)) == sumOfWord(targetWord)) return true;
+    else return false;
+}
