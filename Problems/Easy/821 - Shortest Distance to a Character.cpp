0974814ba36587bcd8ac9304e404cb9426b5ac84
Akash Sahu <akkusahu559@gmail.com>
2021-06-10 10:50:54 +0530

    Java Solution Added

--- Problems/Easy/821 - Shortest Distance to a Character.cpp
+++ Problems/Easy/821 - Shortest Distance to a Character.cpp
@@ -30,3 +30,32 @@ vector<int> shortestToChar(string s, char c) {
     }
     return res;        
 }
+
+
+// 2nd Approach in Java
+// Time Complexity = O(N), Space Complexity = O(N)
+
+public int[] shortestToChar(String s, char c) {
+    ArrayList<Integer> arr = new ArrayList<Integer>();        
+    for(int itr = 0; itr < s.length(); itr++){
+        if(s.charAt(itr) == c) arr.add(itr);
+    }
+    int[] res = new int[s.length()];
+    int dif1 = 0, dif2 = 0;
+    
+    for(int jtr = 0, itr = 0; itr < s.length(); itr++){
+        if(arr.get(jtr) >= itr || jtr+1 == arr.size()) res[itr] = Math.abs(arr.get(jtr)-itr);
+        else{                
+            dif1 = Math.abs(arr.get(jtr+1)-itr);
+            dif2 = Math.abs(arr.get(jtr)-itr);
+
+            if(dif1 <= dif2){
+                res[itr] = dif1;
+                jtr++;
+            }
+            else res[itr] = dif2;
+        }
+    }
+    return res; 
+}
+
