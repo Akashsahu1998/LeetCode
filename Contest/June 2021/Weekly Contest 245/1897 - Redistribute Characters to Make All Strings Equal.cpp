b83e253a0d33c667cd86ec651de10ffddd8d6e60
Akash Sahu <akkusahu559@gmail.com>
2021-06-13 22:59:21 +0530

    Java solution added

--- Contest/June 2021/Weekly Contest 245/1897 - Redistribute Characters to Make All Strings Equal.cpp
+++ Contest/June 2021/Weekly Contest 245/1897 - Redistribute Characters to Make All Strings Equal.cpp
@@ -19,3 +19,23 @@ bool makeEqual(vector<string>& words) {
     return true;
 }
 
+// 2nd Approach in Java
+
+public boolean makeEqual(String[] words) {
+    HashMap<Character, Integer> hmp = new HashMap<Character, Integer>();
+    for(int itr = 0; itr < words.length; itr++){
+        String str = words[itr];
+        int jtr = 0, len = str.length();
+        while(jtr < len){
+            if (hmp.containsKey(str.charAt(jtr)))  hmp.put(str.charAt(jtr), hmp.get(str.charAt(jtr)) + 1);
+            else hmp.put(str.charAt(jtr), 1);
+            jtr++;
+        }
+    }
+    
+    for(Map.Entry entry : hmp.entrySet()){            
+        if((int)entry.getValue()%(words.length) != 0) return false;
+    }
+    return true;
+}
+
