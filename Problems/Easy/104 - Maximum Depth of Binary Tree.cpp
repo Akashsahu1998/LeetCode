40dc4cfb10fd6ee6783137b5c985b143a9fc28b9
Akash <akkusahu559@gmail.com>
2021-08-28 18:38:11 +0530

    Maximum depth BFS approach

--- Problems/Easy/104 - Maximum Depth of Binary Tree.cpp
+++ Problems/Easy/104 - Maximum Depth of Binary Tree.cpp
@@ -11,3 +11,24 @@ int maxDepth(TreeNode* root) {
     if(root == NULL) return 0;
     return max(maxDepth(root->left), maxDepth(root->right))+1;
 }
+
+
+// BFS Approach
+// Time Complexity = O(N), Space Complexity = O(W) where W is the width of the Binary Tree
+int maxDepth(TreeNode* root) {
+    if (!root) return 0;
+    int count = 0;        
+    queue<TreeNode*> q;
+    q.push(root);
+    while(!q.empty()){
+        count++;
+        int size = q.size();
+        for (int i = 0; i < size; i++){
+            TreeNode *cur = q.front();
+            q.pop();
+            if (cur->left) q.push(cur->left);                    
+            if (cur->right) q.push(cur->right);
+        }
+    }        
+    return count;
+}
