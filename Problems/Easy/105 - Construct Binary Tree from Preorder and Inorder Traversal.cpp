3b1516117417cf3bc3c89c59cbf6b81ac8c793df
Akash <akkusahu559@gmail.com>
2021-09-03 08:37:00 +0530

    Construct binary tree from preorder and inorder traversal, Map Approach

--- Problems/Easy/105 - Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ Problems/Easy/105 - Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -27,3 +27,28 @@ TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
     return buildBinaryTree(preorder, inorder, 0, inorder.size()-1, preIndex);
 }
 
+
+// Using Unordered Map Approach 
+// Time Complexity = O(N), Space Complexity = O(N) where N is the size of the inorder vector
+
+TreeNode* buildBinaryTree(vector<int>& preorder, vector<int>& inorder, int is, int ie, unordered_map<int, int> ump, int& preIndex) {
+    if(is > ie) return NULL;
+    TreeNode *root = new TreeNode(preorder[preIndex++]);
+    
+    // get the index of the val;
+    int inIndex = ump[root->val];
+    
+    root->left = buildBinaryTree(preorder, inorder, is, inIndex-1, ump, preIndex);
+    root->right = buildBinaryTree(preorder, inorder, inIndex+1, ie, ump, preIndex);
+    return root;
+}
+
+TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    int preIndex = 0;
+	unordered_map<int, int> ump;    
+    for(int itr = 0; itr < inorder.size(); itr++){
+        ump[inorder[itr]] = itr;
+    }
+    return buildBinaryTree(preorder, inorder, 0, inorder.size()-1, ump, preIndex);
+}
+
