201fc47eb87a0b1200db880f0322c73a4de8b1ef
Akash <akkusahu559@gmail.com>
2021-10-05 08:39:50 +0530

    Efficient approach for counting the number of nodes into the complete binary tree

--- Problems/Medium/222 - Count Complete Tree Nodes.cpp
+++ Problems/Medium/222 - Count Complete Tree Nodes.cpp
@@ -14,3 +14,26 @@ public:
         else return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
+
+
+// Efficient Approach
+// Time Complexity = O(Log(n)^2), Space Complexity = O(H) where H is the height of the Binary Tree
+
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        int leftHeight = 0, rightHeight = 0;
+        TreeNode *cur = root;
+        while(cur != NULL){
+            leftHeight++;
+            cur = cur->left;
+        }
+        cur = root;
+        while(cur != NULL){
+            rightHeight++;
+            cur = cur->right;
+        }
+        if(leftHeight == rightHeight) return pow(2, leftHeight) - 1;
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
