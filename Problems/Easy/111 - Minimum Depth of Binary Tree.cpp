8aacbf5ce9e6c3176cbc5f72f574ccaac371a260
Akash <akkusahu559@gmail.com>
2021-10-12 07:53:34 +0530

    Minimum Depth of Binary Tree using BFS

--- Problems/Easy/111 - Minimum Depth of Binary Tree.cpp
+++ Problems/Easy/111 - Minimum Depth of Binary Tree.cpp
@@ -17,3 +17,29 @@ public:
         : min(minDepth(root->left), minDepth(root->right)) + 1;
     }
 };
+
+
+// BFS Approach
+// Time Complexity = O(N), Space Complexity = O(W) where W is the width of the Binary Tree
+
+class Solution {
+public:    
+    int minDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int countMinDepth = 0;
+        while(!q.empty()){
+            int size = q.size();
+            countMinDepth++;
+            for(int itr = 0; itr < size; itr++){
+                TreeNode* cur = q.front();
+                q.pop();
+                if(cur->left) q.push(cur->left);
+                if(cur->right) q.push(cur->right);
+                if(!cur->left && !cur->right) return countMinDepth;
+            }
+        }
+        return countMinDepth;
+    }
+};
