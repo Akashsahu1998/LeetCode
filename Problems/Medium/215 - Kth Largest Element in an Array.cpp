2477cdf0a0f6c46bfae25135692b3e3eaba5d0a3
Akash <akkusahu559@gmail.com>
2021-12-07 19:16:53 +0530

    Using Bubble Sort

--- Problems/Medium/215 - Kth Largest Element in an Array.cpp
+++ Problems/Medium/215 - Kth Largest Element in an Array.cpp
@@ -16,7 +16,7 @@ public:
 };
 
 
-// 1st Approach
+// 2nd Approach
 // Using Priority Queue
 // Time Complexity = O(NLogN), Space Complexity = O(1)
 class Solution {
@@ -25,5 +25,25 @@ public:
         priority_queue<int> pq(nums.begin(), nums.end());
 	    for(int itr = 1; itr < k; itr++) pq.pop();
 	    return pq.top();
+    }
+};
+
+
+// 3rd Approach
+// Using Bubble Sort
+// Time Complexity = O(N*K), Space Complexity = O(1)
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        for(int itr = 0; itr < k; itr++){
+            for(int jtr = 0; jtr < nums.size()-itr-1; jtr++){
+                if(nums[jtr] > nums[jtr+1]){
+                    int temp = nums[jtr];
+                    nums[jtr] = nums[jtr+1];
+                    nums[jtr+1] = temp;
+                }
+            }            
+        }
+        return nums[nums.size()-k];
     }
 };
