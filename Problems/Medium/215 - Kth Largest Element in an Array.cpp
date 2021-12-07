fa4f499a0e015a844066fc3e1448fe7ee1ef9a15
Akash <akkusahu559@gmail.com>
2021-12-07 19:11:48 +0530

    Using Priority Queue

--- Problems/Medium/215 - Kth Largest Element in an Array.cpp
+++ Problems/Medium/215 - Kth Largest Element in an Array.cpp
@@ -14,3 +14,16 @@ public:
         return nums[k-1];
     }
 };
+
+
+// 1st Approach
+// Using Priority Queue
+// Time Complexity = O(NLogN), Space Complexity = O(1)
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq(nums.begin(), nums.end());
+	    for(int itr = 1; itr < k; itr++) pq.pop();
+	    return pq.top();
+    }
+};
