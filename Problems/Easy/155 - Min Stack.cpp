1bc01cae05b7992700a7b823f81b38584727e159
Akash <akkusahu559@gmail.com>
2021-12-10 06:00:47 +0530

    Min Stack

---
+++ Problems/Easy/155 - Min Stack.cpp
@@ -0,0 +1,42 @@
+
+// Question) 155. Min Stack
+
+
+// Implementation
+
+// Time Complexity = O(1)
+
+class MinStack {
+private:
+    stack<int> s;
+    int min = INT_MAX;
+    
+public:    
+    MinStack() {        
+    }
+    
+    void push(int val) {
+        if(val <= min){
+            s.push(min);    
+            min = val;
+        }        
+        s.push(val);
+    }
+    
+    void pop() {        
+        if(s.top() == min) {
+            s.pop();
+            min = s.top();
+            s.pop();
+        }
+        else s.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return min;
+    }
+};
