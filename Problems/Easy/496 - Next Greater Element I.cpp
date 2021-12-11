9a0d5063ec775b553aae1865a0d8a6a9e2bcc19c
Akash <akkusahu559@gmail.com>
2021-12-11 22:28:50 +0530

    Traversing from start using stack and map

--- Problems/Easy/496 - Next Greater Element I.cpp
+++ Problems/Easy/496 - Next Greater Element I.cpp
@@ -31,3 +31,30 @@ public:
         return res;
     }
 };
+
+
+// 2nd Approach
+// Using Map and Stack
+// Traversing from start
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> s;
+        unordered_map<int, int> mp;
+        vector<int> res;
+        
+        for(int itr = 0; itr < nums2.size(); itr++){
+            while(!s.empty() && nums2[itr] >= s.top()){
+                mp[s.top()] = nums2[itr];
+                s.pop();
+            }
+            s.push(nums2[itr]);
+        }
+        
+        for(int itr = 0; itr < nums1.size(); itr++){
+            mp[nums1[itr]] ? res.push_back(mp[nums1[itr]]) : res.push_back(-1);
+        }
+        
+        return res;
+    }
+};
