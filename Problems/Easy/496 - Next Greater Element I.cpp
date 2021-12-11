35fd6663639c4d9e9483796ab4abfa2335f29acd
Akash <akkusahu559@gmail.com>
2021-12-11 22:28:33 +0530

    Traversing from back using stack and map

---
+++ Problems/Easy/496 - Next Greater Element I.cpp
@@ -0,0 +1,33 @@
+
+// Question) 496. Next Greater Element I
+
+
+// Implementation
+
+// Time Complexity = O(N), Space Complexity = O(N)
+
+// 1st Approach
+// Using Map and Stack
+// Traversing from back
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> s;
+        unordered_map<int, int> mp;
+        vector<int> res;
+        
+        for(int itr = nums2.size()-1; itr >= 0; itr--){
+            while(!s.empty() && s.top() < nums2[itr]){
+                s.pop();
+            }
+            if(s.empty()) mp[nums2[itr]] = -1;
+            else mp[nums2[itr]] = s.top();
+            s.push(nums2[itr]);
+        }
+        
+        for(int itr = 0; itr < nums1.size(); itr++){
+            res.push_back(mp[nums1[itr]]);
+        }
+        return res;
+    }
+};
