3551095ddffd1135b4c1976db79ed31a2e383067
Akash <akkusahu559@gmail.com>
2021-12-14 15:12:16 +0530

    Maximum path sum

---
+++ Problems/Hard/124 - Binary Tree Maximum Path Sum.cpp
@@ -0,0 +1,24 @@
+
+// Question) 124. Binary Tree Maximum Path Sum
+
+
+// Implementation
+
+// DFS Traversal
+// Time Complexity = O(N), Space Complexity = O(H), where H is the height of the Binary Tree.
+class Solution {
+public:
+    int maxPathSumFromBinaryTree(TreeNode* root, int &ans) {
+        if(!root) return 0;
+        int leftSum = max(0, maxPathSumFromBinaryTree(root->left, ans));
+        int rightSum = max(0, maxPathSumFromBinaryTree(root->right, ans));
+        ans = max(ans, leftSum + rightSum + root->val);
+        return max(leftSum, rightSum) + root->val;
+    }
+    
+    int maxPathSum(TreeNode* root) {
+        int ans = INT_MIN;
+        maxPathSumFromBinaryTree(root, ans);
+        return ans;
+    }
+};
