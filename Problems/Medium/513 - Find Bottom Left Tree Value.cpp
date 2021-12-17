467041248d684582404b88fa3ec7e410a40e4eee
Akash Sahu <akkusahu559@gmail.com>
2021-12-17 16:46:44 +0530

    Using level order traversal from left to right

---
+++ Problems/Medium/513 - Find Bottom Left Tree Value.cpp
@@ -0,0 +1,28 @@
+
+// Question) 513. Find Bottom Left Tree Value
+
+
+// Implementation
+
+// Using level order traversal from left to right
+// Time Complexity = O(N), Space Complexity = O(N)
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {        
+        queue<TreeNode*> q;
+        q.push(root);
+        int ans = root->val;
+        
+        while(!q.empty()){
+            int size = q.size();
+            for(int itr = 0; itr < size; itr++){
+                TreeNode *temp = q.front();            
+                q.pop();
+                if(itr == 0) ans = temp->val;                
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+            }
+        }
+        return ans;
+    }
+};
