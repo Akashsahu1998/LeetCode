8a4dee08eaacbb6deb566a88ed96f6b37a75f2bc
Akash Sahu <akkusahu559@gmail.com>
2021-12-18 22:41:41 +0530

    Using two pointer approach

--- Problems/Hard/42 - Trapping Rain Water.cpp
+++ Problems/Hard/42 - Trapping Rain Water.cpp
@@ -28,3 +28,28 @@ public:
         return ans;
     }
 };
+
+
+// Using two pointer approach
+// Time Complexity = O(N), Space Complexity = O(1)
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int ans = 0, left = 0, right = height.size()-1, leftMax = -1, rightMax = -1;
+        
+        while(left < right){
+            if(height[right] >= height[left]){
+                if(height[left] >= leftMax) leftMax = height[left];
+                else ans += leftMax - height[left];
+                left++;
+            }
+            else{
+                if(height[right] >= rightMax) rightMax = height[right];
+                else ans += rightMax - height[right];
+                right--;
+            }
+        }
+        
+        return ans;
+    }
+};
