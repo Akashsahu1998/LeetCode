736d89a4f5f47932f82128cd50c96cd895796b0c
Akash Sahu <akkusahu559@gmail.com>
2021-12-20 22:17:59 +0530

    Using greedy based appoach

---
+++ Problems/Medium/45 - Jump Game II.cpp
@@ -0,0 +1,22 @@
+
+// Question) 45. Jump Game II
+
+
+// Implementation
+
+// Greedy Based Approach
+// Time Complexity = O(N), Space Complexity = O(1)
+class Solution {
+public:    
+    int jump(vector<int>& nums) {
+        int curEnd = 0, maxReachable = 0, jump = 0;
+        for(int itr = 0; itr < nums.size()-1; itr++){
+            maxReachable = max(maxReachable, itr+nums[itr]);
+            if(itr == curEnd){
+                jump++;
+                curEnd = maxReachable;
+            }
+        }
+        return jump;
+    }
+};
