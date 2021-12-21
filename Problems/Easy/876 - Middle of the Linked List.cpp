b00c9806deda8a217e95996145beae512ef6b8ad
Akash Sahu <akkusahu559@gmail.com>
2021-12-21 16:27:00 +0530

    Using slow and fast pointer

---
+++ Problems/Easy/876 - Middle of the Linked List.cpp
@@ -0,0 +1,20 @@
+
+// Question) 876. Middle of the Linked List
+
+
+// Implementation
+
+
+// Slow & Fast Pointer
+// Time Complexity = O(N), Space Complexity = O(1)
+class Solution {
+public:
+    ListNode* middleNode(ListNode* slow) {
+        ListNode* fast = slow;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
