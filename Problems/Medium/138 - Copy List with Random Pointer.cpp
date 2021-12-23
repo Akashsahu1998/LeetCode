41121982498b684fc070b76b2c138bc6ac70ca95
Akash Sahu <akkusahu559@gmail.com>
2021-12-23 07:17:14 +0530

    Efficient Approach

--- Problems/Medium/138 - Copy List with Random Pointer.cpp
+++ Problems/Medium/138 - Copy List with Random Pointer.cpp
@@ -45,3 +45,50 @@ public:
         return copy->next;
     }
 };
+
+
+// Efficient Approach
+// Time Complexity = O(N), Space Complexity = O(1)
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head) return NULL;
+        
+        // Building the dummyNode after each and every original node(cur)
+        // pointing the cur->next to dummyNode & dummyNode to cur->next->next
+        Node *cur = head;        
+        while(cur){
+            Node *front = cur->next;
+            Node *dummyNode = new Node(cur->val);
+            cur->next = dummyNode;
+            dummyNode->next = front;
+            cur = front;
+        }
+        
+        // Pointing the randomNode in each and every dummy node we created
+        cur = head;
+        while(cur){
+            if(cur->random){
+                // we are putting random node(cur->random->next) in dummyNode(cur->next->random) only
+                cur->next->random = cur->random->next;
+            }
+            cur = cur->next->next;
+        }
+        
+        // Pointing the copy to the first dummy node, so we can return it.
+        cur = head;
+        Node* copy = new Node(0);
+        Node* copyCur = copy;
+        
+        // deleting the links of org nodes to dummy nodes
+        while(cur){            
+            copyCur->next = cur->next;
+            cur->next = cur->next->next;
+            cur = cur->next;
+            copyCur = copyCur->next;
+        }
+        
+        return copy->next;
+    }
+};
+// https://www.youtube.com/watch?v=VNf6VynfpdM
