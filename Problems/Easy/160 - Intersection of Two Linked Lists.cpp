abb857d2d586548657604f6601a7b86de3a1272b
Akash Sahu <akkusahu559@gmail.com>
2021-12-23 08:19:01 +0530

    Efficient approach with comments

--- Problems/Easy/160 - Intersection of Two Linked Lists.cpp
+++ Problems/Easy/160 - Intersection of Two Linked Lists.cpp
@@ -31,3 +31,28 @@ public:
         return NULL;
     }
 };
+
+// Efficient Approach
+// Idea is to iterate over a list two times
+// Time Complexity = O(N), Space Complexity = O(1)
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* curA = headA;
+        ListNode* curB = headB;
+        
+        // For example
+        // headA = [1, 2, 3, 4]
+        // headB = [8, 4]
+        // Just assume it as
+        // headA = [1, 2, 3, 4, 8, 4]
+        // headB = [8, 4, 1, 2, 3, 4]
+        // we are iterating in this way
+        
+        while(curA != curB){
+            curA = curA ? curA->next : headB;
+            curB = curB ? curB->next : headA;
+        }
+        return curA;
+    }
+};
