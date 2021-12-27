aea7a46a5f3a7c967ac3919ca3baedb03b4667cc
Akash Sahu <akkusahu559@gmail.com>
2021-12-27 08:27:19 +0530

    Using Stack

---
+++ Problems/Easy/20. Valid Parentheses.cpp
@@ -0,0 +1,31 @@
+
+// Question) 1. Two Sum
+
+
+// Implementation
+
+// Using Stack
+// Time Complexity = O(N), Space Complexity = O(N)
+class Solution {
+public:
+    bool isValid(string str) {
+        stack<char> s;
+        int itr = 0;
+        
+        while(itr < str.size()){
+            if(s.empty()){
+                s.push(str[itr]);
+            }
+            else{
+                if(str[itr] == '}' && s.top() == '{') s.pop();
+                else if(str[itr] == ']' && s.top() == '[') s.pop();
+                else if(str[itr] == ')' && s.top() == '(') s.pop();
+                else s.push(str[itr]);
+            }
+            itr++;
+        }
+        
+        if(s.empty()) return true;
+        return false;
+    }
+};
