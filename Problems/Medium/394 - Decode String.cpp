6d7ae13110e08681bbaf9a1d706c23a367cf9f92
Akash Sahu <akkusahu559@gmail.com>
2021-12-31 09:08:57 +0530

    Efficient approach using, 1 stacks

--- Problems/Medium/394 - Decode String.cpp
+++ Problems/Medium/394 - Decode String.cpp
@@ -54,3 +54,51 @@ public:
         return res;
     }
 };
+
+
+
+// 2nd
+// Efficient Approach
+// Using 1 stack
+// Time Complexity = O(N), Space Complexity = O(N)
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<char> st;   // char stack        
+        for(int itr = 0; itr < s.size(); itr++){
+            if(s[itr] == ']'){
+                string str = "";
+                while(st.top() != '['){
+                    str = st.top() + str;
+                    st.pop();
+                }                
+                st.pop();
+                
+                string times = "";
+                while(!st.empty() && isdigit(st.top())){
+                    times = st.top() + times;
+                    st.pop();
+                }
+                
+                int count = stoi(times);
+                
+                while(count--){
+                    for(int jtr = 0; jtr < str.size(); jtr++){
+                        st.push(str[jtr]);
+                    }
+                }
+            }
+            else{
+                st.push(s[itr]);
+            }
+        }
+        
+        string res = "";
+        while(!st.empty()){
+            res += st.top();
+            st.pop();
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
