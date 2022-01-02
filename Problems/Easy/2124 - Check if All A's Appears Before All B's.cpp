2c956cd771a1afdf59bae14a449ec51018f24e26
Akash Sahu <akkusahu559@gmail.com>
2022-01-02 13:29:21 +0530

    Just simply checking the [index-1] value

--- Problems/Easy/2124 - Check if All A's Appears Before All B's.cpp
+++ Problems/Easy/2124 - Check if All A's Appears Before All B's.cpp
@@ -18,3 +18,16 @@ public:
         return true;
     }
 };
+
+
+// Just simply checking the [index-1] value
+// Time Complexity = O(N), Space Complexity = O(N)
+class Solution {
+public:
+    bool checkString(string str) {       
+        for(int itr = 1; itr < str.size(); itr++){
+            if(str[itr] == 'a' && str[itr-1] == 'b') return false;
+        }
+        return true;
+    }
+};
