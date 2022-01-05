037b8d9041c25604e4678c54708932de37b114da
Akash Sahu <akkusahu559@gmail.com>
2022-01-05 06:34:10 +0530

    Efficient Approach

--- Problems/Hard/132 - Palindrome Partitioning II.cpp
+++ Problems/Hard/132 - Palindrome Partitioning II.cpp
@@ -4,7 +4,7 @@
 
 // Implementation
 
-// Using Gap Strategy
+// Using Gap & Cut Strategy
 // Will give TLE
 // Naive Approach
 // Time Complexity = O(N^3), Space Complexity = O(N^2)
@@ -51,5 +51,51 @@ public:
         
         // returning the last index of the first row
         return res[0][n-1];
+    }
+};
+
+
+
+// Using Gap & Cut Strategy
+// Efficient Approach
+// Time Complexity = O(N^2), Space Complexity = O(N^2)
+class Solution {
+public:
+    int minCut(string str) {
+        int n = str.size();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+                
+        for(int gap = 0; gap < n; gap++){
+            for(int itr = 0, jtr = gap; jtr < n; itr++, jtr++){
+                if(gap == 0) dp[itr][jtr] = true;
+                else if(gap == 1) dp[itr][jtr] = str[itr] == str[jtr];
+                else{
+                    if(str[itr] == str[jtr] && dp[itr+1][jtr-1]) dp[itr][jtr] = true;
+                    else dp[itr][jtr] = false;
+                }
+            }
+        }
+        
+        // using suffix technique
+        vector<int> res(n);
+        res[0] = 0;     // first char will always be pallindrome
+        
+        for(int itr = 1; itr < n; itr++){
+            if(dp[0][itr]) res[itr] = 0;
+            else{
+                int minCut = INT_MAX;
+                
+                for(int jtr = itr; jtr >= 1; jtr--){                    
+                    // if its pallindrome then only, we can find how many cut we can make
+                    if(dp[jtr][itr]){
+                        minCut = min(minCut, res[jtr-1]);
+                    }
+                }
+                res[itr] = minCut + 1;  // +1 bcz we are making cut on current char as well
+            }
+        }
+        
+        // returning the last index of the res
+        return res[n-1];
     }
 };
