438810f5c66fb28b85b352142be8ced5c2dc311b
Akash Sahu <akkusahu559@gmail.com>
2022-01-06 15:39:39 +0530

    By sorting the vector

--- Problems/Medium/1094 - Car Pooling.cpp
+++ Problems/Medium/1094 - Car Pooling.cpp
@@ -30,3 +30,35 @@ public:
         return true;
     }
 };
+
+
+
+// 2nd approach
+// By sorting the vector 
+// Time Complexity = O(NLogN), Space Complexity = O(N)
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+	    vector<pair<int, int>> vec;
+                
+        for(int itr = 0; itr < trips.size(); itr++){
+        	// we are storing the source and number of people who will enter on this source
+            vec.push_back({trips[itr][1], trips[itr][0]});
+            
+            // we are storing the destination and number of people who will exit that's why '-' symbol it means these number of people will go out of car.
+            vec.push_back({trips[itr][2], -trips[itr][0]});
+        }
+        
+        // sorting the vector bcz car will move in one direction, so we want the source to destination in increasing order like, 1 2 3 4 5 6 7 8 9....
+        sort(vec.begin(), vec.end());
+        
+        // we are just subtracting the number of people from capacity, if there are more people greater than the capacity than
+		// our capacity will go less than 0, so we will return false
+        for(auto a : vec){
+            capacity -= a.second;
+            if(capacity < 0) return false;
+        }
+        
+        return true;
+    }
+};
