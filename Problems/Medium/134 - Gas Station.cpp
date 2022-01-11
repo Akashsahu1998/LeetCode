6c761e6432ba64b833b3a524fc429740e80b051b
Akash Sahu <akkusahu559@gmail.com>
2022-01-11 22:47:32 +0530

    Most Efficient Approach

--- Problems/Medium/134 - Gas Station.cpp
+++ Problems/Medium/134 - Gas Station.cpp
@@ -31,3 +31,25 @@ public:
        return start;
     }
 };
+
+
+// 2nd Approach
+// Most Efficient Approach
+// Time Complexity = O(N), Space Complexity = O(1)
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {        
+        int start = 0, backupTotal = 0, total = 0;
+        
+        for(int itr = 0; itr < gas.size(); itr++){
+            backupTotal = backupTotal + gas[itr] - cost[itr];
+            if(backupTotal < 0){
+                start = itr+1;
+                total += backupTotal;
+                backupTotal = 0;
+            }
+        }      
+       
+        return (total + backupTotal) >= 0 ? start : -1 ;
+    }
+};
