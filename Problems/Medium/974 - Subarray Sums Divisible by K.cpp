c8029d8b44e095770a0a1e2a91971c157cd3ded1
Akash Sahu <akkusahu559@gmail.com>
2022-01-15 14:26:54 +0530

    Using Vector & Prefix Sum

--- Problems/Medium/974 - Subarray Sums Divisible by K.cpp
+++ Problems/Medium/974 - Subarray Sums Divisible by K.cpp
@@ -4,7 +4,7 @@
 
 // Implementation
 
-// Using unodered_map
+// Using unodered_map & prefix sum
 // Time Complexity = O(N), Space Complexity = O(N)
 class Solution {
 public:
@@ -26,6 +26,39 @@ public:
             if(sumMod < 0) sumMod += k;
             
             // add the frequency of sumMod into count, if its not present into map, then 0 will get added, otherwise the frequency of sumMod key will get added            
+            count += mp[sumMod];
+            
+            // increase the frequency of sumMod into map by 1
+            mp[sumMod]++;
+        }
+        return count;
+    }
+};
+
+
+
+// Using Vector & Prefix Sum
+// Time Complexity = O(N), Space Complexity = O(N)
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+         // taking a vector as a map to calculate the frequency if a remainder
+        vector<int> mp(k+1, 0);
+        mp[0] = 1;
+        int sum = 0, count = 0;
+        
+        for(int itr = 0; itr < nums.size(); itr++){
+            // adding current element into sum
+            sum += nums[itr];
+            
+            // finding the remainder of the sum
+            int sumMod = sum % k;
+            
+            // to handle the negative sumMod case, i.e nums[-1,2,9], k = 2
+            // whenver sumMod is less than 0, then add k into it
+            if(sumMod < 0) sumMod += k;
+            
+            // add the frequency of sumMod into count, if its not present into vector, then 0 will get added, otherwise the frequency of sumMod will get added            
             count += mp[sumMod];
             
             // increase the frequency of sumMod into map by 1
