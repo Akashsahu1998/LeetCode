52c5f0745be819dd51b2f4828143d070493d041a
Akash Sahu <akkusahu559@gmail.com>
2022-01-15 13:00:42 +0530

    Using unodered_map

--- Problems/Medium/523 - Continuous Subarray Sum.cpp
+++ Problems/Medium/523 - Continuous Subarray Sum.cpp
@@ -4,6 +4,7 @@
 
 // Implementation
 
+// 1st
 // Naive Approach
 // Giving TLE
 // Time Complexity = O(N^2), Space Complexity = O(1)
@@ -19,6 +20,40 @@ public:
             for(int jtr = 0; jtr < itr-1; jtr++){
                 temp -= nums[jtr];
                 if(temp % k == 0) return true;
+            }
+        }
+        return false;
+    }
+};
+
+
+
+// 2nd
+// Efficient Approach
+// Using unodered_map
+// Time Complexity = O(N), Space Complexity = O(N)
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        
+        // taking a map to store the key as a remainder and value as a current index+1
+        unordered_map<int, int> mp;        
+        mp[0] = 0;
+        int sum = 0;
+        
+        for(int itr = 0; itr < nums.size(); itr++){
+            // adding current element into sum
+            sum += nums[itr];
+            
+            // finding the remainder of the sum
+            int sumMod = sum % k;
+            
+            // if remainder is present into map, then check the index of the present remainder into map, and the current index, if the length is more than or equal to 2, it means, we got the subarray of minimum length 2 
+            if(mp.find(sumMod) != mp.end()){
+                if(itr+1 - mp[sumMod] >= 2) return true;
+            }
+            else{   // if remainder is not present into map, then just put that remainder as a key and value as current index+1;
+                mp[sumMod] = itr+1;
             }
         }
         return false;
