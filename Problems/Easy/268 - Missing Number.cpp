463a13e0fc1e268f8be70a80b721573c7e194d76
Akash Sahu <akkusahu559@gmail.com>
2022-01-22 16:53:32 +0530

    Using XOR approach

--- Problems/Easy/268 - Missing Number.cpp
+++ Problems/Easy/268 - Missing Number.cpp
@@ -28,3 +28,33 @@ public:
 };
 
 
+
+
+// 2nd Approach
+// Best Approach
+// Using XOR approach 
+// Time Complexity = O(N)
+// Space Complexity = O(1)
+// Approach,  suppose i.e arr = [1,2,3,5], n = 5, and our 1 to n elements are = 1,2,3,4,5
+// so, 1^1 is 0, so in this way if we will xor all arr elements with natural elements only 4 will left
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        
+        // array elements xor
+        int xorArrEle = nums[0];
+        for(int itr = 1; itr < n; itr++){
+            xorArrEle = xorArrEle ^ nums[itr];
+        }
+        
+        // natural elements xor
+        int xorNaturalEle = 1;
+        for(int itr = 2; itr <= n; itr++){
+            xorNaturalEle = xorNaturalEle ^ itr;
+        }
+        
+        return xorArrEle ^ xorNaturalEle;
+    }
+};
+
