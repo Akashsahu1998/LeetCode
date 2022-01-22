8dfe74f3c67c711c18380fedeb8aa32b000765a7
Akash Sahu <akkusahu559@gmail.com>
2022-01-22 13:41:01 +0530

    Using Sqrt function

---
+++ Problems/Easy/69 - Sqrt(x).cpp
@@ -0,0 +1,17 @@
+
+// Question) 69. Sqrt(x)
+
+
+// Implementation
+
+
+// 1st Approach
+// Using Sqrt function
+// Time Complexity = O(sqrt(n))
+// Space Complexity = O(1)
+// class Solution {
+// public:
+//     int mySqrt(int x) {
+//         return sqrt(x);
+//     }
+// };
