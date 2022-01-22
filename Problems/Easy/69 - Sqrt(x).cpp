50f5a941c9f4e7545a1b47b80c923413d1389ebe
Akash Sahu <akkusahu559@gmail.com>
2022-01-22 13:41:17 +0530

    2nd Approach

--- Problems/Easy/69 - Sqrt(x).cpp
+++ Problems/Easy/69 - Sqrt(x).cpp
@@ -15,3 +15,26 @@
 //         return sqrt(x);
 //     }
 // };
+
+
+ 2nd Approach
+ Time Complexity = O(sqrt(n))
+ Space Complexity = O(1)
+ class Solution {
+ public:
+     int mySqrt(int x) {
+         int res = 0;
+         for(long long int itr = 1; itr <= x; itr++){
+             long long int val = itr*itr;
+             if(val == x){
+                 res = itr;
+                 break;
+             }
+             else if(val > x){
+                 res = itr-1;
+                 break;
+             }
+         }
+         return res;
+     }
+ };
