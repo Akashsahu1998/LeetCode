c82540ff79386abcb4043a998f4107238a0f1591
Akash Sahu <akkusahu559@gmail.com>
2022-01-30 14:21:41 +0530

    Efficient Approach, 2nd Approach

--- Problems/Medium/2155 - All Divisions With the Highest Score of a Binary Array.cpp
+++ Problems/Medium/2155 - All Divisions With the Highest Score of a Binary Array.cpp
@@ -80,3 +80,46 @@ public:
         return res;
     }
 };
+
+
+// 2nd Approach
+// Time Complexity = O(N)
+// Space Complexity = O(1), not considering res vector, bcz its a part of output
+
+class Solution {
+public:
+    vector<int> maxScoreIndices(vector<int>& nums) {
+        
+        int n = nums.size(), maxAns = INT_MIN, countLeftZero = 0;
+        
+        // getting the count of 1 in the array
+        int countRightOne = accumulate(nums.begin(), nums.end(), 0);
+        
+        // to store the result
+        vector<int> res;
+        
+        // iterating over the nums
+        for(int itr = 0; itr <= n; itr++){
+            
+            // if count of the 0 on left side + count of the 1 on right side is more than maxAns, then clear the res vector and push the current index into it.
+            if((countLeftZero + countRightOne) > maxAns){
+                maxAns = countLeftZero + countRightOne;
+                res.clear();
+                res.push_back(itr);
+            }   
+            
+            // if count of the 0 on left side + count of the 1 on right side is equal to maxAns, then push the current index into it.
+            else if((countRightOne + countLeftZero) == maxAns) res.push_back(itr);
+                        
+            if(itr != n){
+                
+                // if current value is 0 then increase the count of 0 on the left side
+                if(nums[itr] == 0) countLeftZero++;
+                
+                // if current value is 1 then decrease the count of 1 on the right side
+                if(nums[itr] == 1) countRightOne--;
+            }
+        }
+        return res;
+    }
+};
