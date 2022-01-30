a71f0356177767d4ad70e63d8201d64510285938
Akash Sahu <akkusahu559@gmail.com>
2022-01-30 16:40:02 +0530

    Efficient Approach

--- Problems/Medium/2156 - Find Substring With Given Hash Value.cpp
+++ Problems/Medium/2156 - Find Substring With Given Hash Value.cpp
@@ -7,7 +7,7 @@
 // Naive Approach
 // Give TLE
 // Time Complexity = O(N * K * LogN)
-// Space Complexity = O(1), not considering res vector, bcz its a part of output
+// Space Complexity = O(1)
 
 class Solution {
 public:
@@ -16,6 +16,7 @@ public:
     // using this way bcz of overflow issue
     // in this approach, everytime we are doing modulo, so it will never overflow
     // in pow directly we can't do modulo
+	// it will give the result into O(logN)
     unsigned long long int findPower(int power, int jtr, int modulo){
         if(jtr == 0) return 1;
         if(jtr == 1) return power%modulo;
@@ -50,5 +51,79 @@ public:
             }
         }
         return "";
+    }
+};
+
+
+
+// https://www.youtube.com/watch?v=P7dX86HdNqc
+// Efficient Approach
+// Using sliding window technique and finding the power of numbers efficiently
+// Time Complexity = O(N * LogN)
+// Space Complexity = O(1)
+class Solution {
+public:
+    
+    // find the power of numbers in efficient way
+    // using this way bcz of overflow issue
+    // in this approach, everytime we are doing modulo, so it will never overflow
+    // in pow directly we can't do modulo
+    // it will give the result into O(logN)
+    long long findPower(int power, int jtr, int modulo){
+        if(jtr == 0) return 1;
+        if(jtr == 1) return (power%modulo);
+        
+        long long ans = 1;
+        long long temp = findPower(power, jtr/2, modulo) % modulo;
+        
+        // if jtr is odd
+        if(jtr % 2){
+            ans = (ans * power) % modulo;            
+        }
+        
+        return (ans * ((temp * temp) % modulo)) % modulo;
+    }
+    
+    string subStrHash(string s, int power, int modulo, int k, int hashValue) {        
+        reverse(s.begin(), s.end());
+        long long ans = 0;
+        string result;
+        
+        // traversing over the string
+        for(int itr = 0; itr < s.size(); itr++){
+            
+            // we will run this, until & unless we reach to k window
+            if(itr < k){                
+                long long val = s[itr] - 'a' + 1;
+                long long powVal = (findPower(power, k-1-itr, modulo)) % modulo;
+                ans = (ans + ((val * powVal) % modulo)) % modulo;
+                continue;
+            }
+            
+            // if we got the ans 
+            if(ans == hashValue){
+                string ansStr = s.substr(itr-k, k);
+                result = ansStr;
+            }
+            
+            // not got the result yet, so remove the char from the window
+            long long val = s[itr-k] - 'a' + 1;
+            long long powVal = (findPower(power, k-1, modulo)) % modulo;
+            ans = (ans + modulo - ((val * powVal) % modulo)) % modulo;
+            
+            // add the new char into the window now, that's why directly multiplying ans by power
+            ans = (ans * power) % modulo;
+            ans = (ans + (s[itr] - 'a' + 1)) % modulo;
+        }
+        
+        // if we got the ans in the last index 
+        if(ans == hashValue){
+            string ansStr = s.substr(s.size()-k, k);
+            result = ansStr;
+        }
+        
+        // reversing the result, bcz in the starting we reversed the original string(s)
+        reverse(result.begin(), result.end());
+        return result;
     }
 };
