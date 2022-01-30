9c7f8e7f66b6819216310f25a5fb1f23880b985b
Akash Sahu <akkusahu559@gmail.com>
2022-01-30 13:10:20 +0530

    Efficient Approach

--- Problems/Easy/2154 - Keep Multiplying Found Values by Two.cpp
+++ Problems/Easy/2154 - Keep Multiplying Found Values by Two.cpp
@@ -19,3 +19,25 @@ public:
         return original;
     }
 };
+
+
+// Efficient Approach
+// Using Unordered Set
+// Time Complexity = O(N)
+// Space Complexity = O(N)
+
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        unordered_set<int> st;
+        for(int a : nums){
+            st.insert(a);
+        }
+        
+        for(int a : nums){
+            if(st.find(original) != st.end()) original *= 2;
+        }
+        
+        return original;
+    }
+};
