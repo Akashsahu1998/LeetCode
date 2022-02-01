ea22bc0a1e23fd11fd25ba2b381609d96cc2dc85
Akash Sahu <akkusahu559@gmail.com>
2022-02-01 06:38:23 +0530

    Efficient Approach, Traversing 2 times over the array

--- Problems/Medium/75. Sort Colors.cpp
+++ Problems/Medium/75. Sort Colors.cpp
@@ -4,8 +4,9 @@
 
 // Implementation
 
-// Using the inbuilt sorting function
+// 1st Approach
 // Naive Approach
+// Using the inbuilt sort function
 // Time Complexity = O(NLogN)
 // Space Complexity = O(1)
 
@@ -13,5 +14,31 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         sort(nums.begin(), nums.end());
+    }
+};
+
+
+// 2nd Approach
+// Efficient Approach
+// Traversing 2 times
+// Time Complexity = O(N)
+// Space Complexity = O(1)
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int countZero = 0, countOne = 0, countTwo = 0;
+        
+        for(int itr = 0; itr < nums.size(); itr++){
+            if(nums[itr] == 0) countZero++;
+            else if(nums[itr] == 1) countOne++;
+            else countTwo++;
+        }
+        
+        for(int itr = 0; itr < nums.size(); itr++){
+            if(itr < countZero) nums[itr] = 0;
+            else if(itr < countZero + countOne) nums[itr] = 1;
+            else nums[itr] = 2;
+        }
     }
 };
