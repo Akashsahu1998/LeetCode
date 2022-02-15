a3d7e269413a2d11652fcadd4d5733c43093d781
Akash Sahu <akkusahu559@gmail.com>
2022-02-15 09:27:27 +0530

    2 Approaches

--- Problems/Medium/131 - Palindrome Partitioning.cpp
+++ Problems/Medium/131 - Palindrome Partitioning.cpp
@@ -4,8 +4,10 @@
 
 // Implementation
 
+// 1st Approach
 // DFS, Backtracking
-// Time Complexity = O(N^2), Space Complexity = O(N), not considering 2-D vector, bcz that's a part of output
+// Time Complexity = O(2^N)
+// Space Complexity = O(N), not considering 2-D vector, bcz that's a part of output
 class Solution {
 public:
     bool isPallindrome(int start, int end, string str){
@@ -37,3 +39,55 @@ public:
         return res;
     }
 };
+
+
+
+// 2nd Approach
+// Using DFS & Backtracking
+// Time Complexity = O(2^n)
+// Space Complexity = O(N), not considering 2-D vector, bcz that's a part of output
+class Solution {
+public:
+    
+    bool isPallindrome(string str){
+        int left = 0, right = str.size()-1;        
+        while(left < right){
+            if(str[left++] != str[right--]) return false;
+        }
+        return true;
+    }
+    
+    void findPartition(vector<vector<string>> &res, vector<string> path, string str){
+        if(!str.size()){
+            res.push_back(path);
+            return;
+        }
+        
+        for(int itr = 0; itr < str.size(); itr++){
+            string tempStr = str.substr(0, itr+1);
+            string ros = str.substr(itr+1);
+            
+            if(isPallindrome(tempStr)){
+                path.push_back(tempStr);
+                findPartition(res, path, ros);
+                path.pop_back();    // backtracking
+            }
+        }
+    }
+    
+    vector<vector<string>> partition(string str) {
+        vector<vector<string>> res;
+        vector<string> path;
+        findPartition(res, path, str);
+        return res;
+    }
+};
+
+
+// Understanding Time Complexity 
+// In the worst case (string is all equal characters, eg "aaaaaaa"), the DP solution depends on the previous solutions, which means the result space is
+
+// f(n) = f(n-1) + f(n-2) + ... + f(1)
+// f(n+1) = f(n) + f(n-1) + f(n-2) + ... + f(1)= 2*f(n)
+// f(n+1)/f(n) = 2
+// So, it's exponential O(2^n)
