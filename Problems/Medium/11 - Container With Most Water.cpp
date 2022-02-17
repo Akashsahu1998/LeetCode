2f655613030067c1abfbb5fb49b11c21cd300135
Akash Sahu <akkusahu559@gmail.com>
2022-02-17 15:34:59 +0530

    Efficient Approach, Using 2 pointer's Approach

--- Problems/Medium/11 - Container With Most Water.cpp
+++ Problems/Medium/11 - Container With Most Water.cpp
@@ -24,3 +24,43 @@ public:
         return maxAns;
     }
 };
+
+
+// Efficient Approach
+// Using Two Pointer's Approach
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        
+        // taking maxAns to store the ans
+        int maxAns = INT_MIN;
+        
+        // taking two pointers
+        int start = 0, end = height.size()-1;
+        
+        // run untill and unless start become greater than or equal to end
+        while(start < end){
+            
+            // taking the minimum of both start and end index value
+            int minVal = min(height[start], height[end]);
+            
+            // calculating the distance b/w the start & end & multiplying it with minVal
+            int total = minVal * (end - start);
+            
+            // storing the max ans 
+            maxAns = max(maxAns, total);
+            
+            // if end value is greater than start, just increment start by 1
+            if(height[start] < height[end]){
+                start++;
+            }
+            else{   // otherwise decrement end by 1
+                end--;
+            }
+        }
+        
+        return maxAns;
+    }
+};
