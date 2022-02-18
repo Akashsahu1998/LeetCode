311f0004f3f4029c10d3f8b7a44a8192d4589a98
Akash Sahu <akkusahu559@gmail.com>
2022-02-18 20:21:19 +0530

    2nd Approach

--- Problems/Easy/9 - Palindrome Number.cpp
+++ Problems/Easy/9 - Palindrome Number.cpp
@@ -29,3 +29,32 @@ public:
         return true;
     }
 };
+
+
+// 2nd Approach
+// Idea: reverse the half number
+// Time Complexity: O(LogN)
+// Space Complexity: O(1)
+class Solution {
+public:    
+    bool isPalindrome(int x) {
+        
+        // (x < 0) -> bcz, anyone value which is less than 0, its means false
+        // (x != 0) -> bcz, for this case x = 0, in this case, it should return true, bcz that is pallindrome
+        // (x%10 == 0) -> bcz, for these cases, x = 10, x = 20
+        if(x < 0 || (x != 0 && x % 10 == 0)) return false;
+        
+        // will store the reverse of the number
+        int rev = 0;
+        
+        // running till the half of the number, so in this way, we can handle the overflow case also
+        while(x > rev){
+            rev = (rev * 10) + (x % 10);
+            x /= 10;
+        }
+        
+        // if x == rev, means both half are equal, i.e, x = 1331
+        // (rev/10 == x), means if the num is odd, i.e, x = 13331
+        return (x == rev) || (rev/10 == x);
+    }
+};
