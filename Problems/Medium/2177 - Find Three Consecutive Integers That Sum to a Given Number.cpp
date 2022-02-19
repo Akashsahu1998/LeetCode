e6f3027d2b49c3b7b48836902227f8fd160d734b
Akash Sahu <akkusahu559@gmail.com>
2022-02-19 23:53:00 +0530

    Find Three Consecutive Integers That Sum to a Given Number

---
+++ Problems/Medium/2177 - Find Three Consecutive Integers That Sum to a Given Number.cpp
@@ -0,0 +1,32 @@
+
+// Question) 2177. Find Three Consecutive Integers That Sum to a Given Number
+
+
+// Implementation
+
+// Time Complexity = O(1)
+// Space Complexity = O(1), not considering the res vector, bcz its a part of result
+class Solution {
+public:
+    vector<long long> sumOfThree(long long num) {
+        
+        
+        vector<long long> res;
+        // for(long long itr = -1; itr <= num/3; itr++){
+        //     long long sum = itr + (itr+1) + (itr+2);
+        //     if(sum == num){
+        //         res.push_back(itr);
+        //         res.push_back(itr+1);
+        //         res.push_back(itr+2);
+        //     }
+        // }
+        
+        long long sum = num/3;
+        if(((sum-1) + sum + (sum+1)) == num){
+            res.push_back(sum-1);
+            res.push_back(sum);
+            res.push_back(sum+1);
+        }
+        return res;
+    }
+};
