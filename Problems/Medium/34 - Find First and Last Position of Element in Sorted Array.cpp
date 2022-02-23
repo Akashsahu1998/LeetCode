78700b11454dc8ff8ebac078ad1be8f17b0d4981
Akash Sahu <akkusahu559@gmail.com>
2022-02-23 22:02:14 +0530

    Efficient Approach

--- Problems/Medium/34 - Find First and Last Position of Element in Sorted Array.cpp
+++ Problems/Medium/34 - Find First and Last Position of Element in Sorted Array.cpp
@@ -5,6 +5,7 @@
 // Implementation
 
 // 1st Approach
+// Brute Force
 // Time Complexity = O(N)
 // Space Complexity = O(1)
 
@@ -32,5 +33,40 @@ public:
         while(last < nums.size()-1 && nums[last] == nums[last+1]) last++;
         
         return {first, last};
+    }
+};
+
+
+// 2nd Approach
+// Efficient Approach
+// Time Complexity = O(LogN)
+// Space Complexity = O(1)
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        if(!nums.size()) return {-1,-1};
+        
+        int low = 0, high = nums.size()-1;        
+        int firstPos = -1, lastPos = -1;
+        
+        while(low < high){
+            int mid = low + (high-low)/2;
+            
+            if(nums[mid] < target) low = mid+1;
+            else high = mid;
+        }        
+        if(nums[low] != target) return {-1,-1};
+        firstPos = low;
+        
+        // only change high, don't change low
+        high = nums.size()-1;
+        while(low < high){
+            int mid = (low + (high-low)/2) + 1;
+            
+            if(nums[mid] > target) high = mid-1;
+            else low = mid;
+        }
+        lastPos = high;
+        return {firstPos, lastPos};
     }
 };
