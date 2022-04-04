232d9ba720bcacffd0a65fdad7d66452aed07c47
Akash Sahu <akkusahu559@gmail.com>
2022-04-04 21:54:57 +0530

    Using slow & fast pointers

--- Problems/Medium/287 - Find the Duplicate Number.cpp
+++ Problems/Medium/287 - Find the Duplicate Number.cpp
@@ -21,3 +21,27 @@ public:
         return -1;
     }
 };
+
+
+// Using slow & fast pointers
+// Time Complexity = O(N)
+// Space Complexity = O(1)
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0], fast = nums[0];
+        
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+        
+        fast = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
