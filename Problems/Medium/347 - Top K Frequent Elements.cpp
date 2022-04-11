ad9c3ee31aad8aaf1bc2dfe9c2dd5bfee2dff9d2
Akash Sahu <akkusahu559@gmail.com>
2022-04-11 21:21:43 +0530

    Multi Set (Min Heap) & Unordered Map

---
+++ Problems/Medium/347 - Top K Frequent Elements.cpp
@@ -0,0 +1,37 @@
+
+// Question) 347. Top K Frequent Elements
+
+
+// Implementation
+
+// Using Multi Set (Min Heap) & Unordered Map
+// Time Complexity = O(NLogK)
+// Space Complexity = O(N)
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        unordered_map<int, int> freq;
+        
+        for(auto num : nums){
+            freq[num]++;
+        }
+        
+        multiset<pair<int, int>> mset;
+        for(auto mp : freq){
+            mset.insert(make_pair(mp.second, mp.first));
+            
+            if(mset.size() > k){
+                mset.erase(mset.begin());
+            }
+        }
+        
+        vector<int> res;        
+        for(auto a : mset){
+            res.push_back(a.second);
+        }
+        
+        return res;
+    }
+};
