c4881cc1872e11c24b28f18b0f9a6585855f111f
Akash Sahu <akkusahu559@gmail.com>
2022-04-14 22:20:53 +0530

    Using DFS

--- Problems/Easy/572 - Subtree of Another Tree.cpp
+++ Problems/Easy/572 - Subtree of Another Tree.cpp
@@ -14,7 +14,7 @@ private:
         if(root == NULL || subRoot == NULL){
             return root == NULL && subRoot == NULL;
         }
-        else if(root->val == subRoot->val){
+        else if(root->val == subRoot->val){	// if root is matching then its left/right child should also match
             return isMatchingTree(root->left, subRoot->left) && isMatchingTree(root->right, subRoot->right);
         }
         else return false;
@@ -22,10 +22,11 @@ private:
     
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+    	// if root is null then subRoot will never be subtree of root
         if(!root) return false;
         else if(isMatchingTree(root, subRoot)){
             return true;
-        }
+        }	// if not matching check its left/right side, bcz there may be subtree can be present
         else return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
     }
 };
