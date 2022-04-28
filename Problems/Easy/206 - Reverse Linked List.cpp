83b78555b668f5d60fefb01cb0bb90d45cd8ca00
Akash Sahu <akkusahu559@gmail.com>
2022-04-28 22:03:39 +0530

    2 Approaches

--- Problems/Easy/206 - Reverse Linked List.cpp
+++ Problems/Easy/206 - Reverse Linked List.cpp
@@ -4,36 +4,42 @@
 
 // Implementation
 
-// Iterative Approach
-// Time Complexity = O(N), Space Complexity = O(1)
-
-class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {        
-        ListNode* prev = NULL;
-        ListNode* next = NULL;        
-        while(head){
-            next = head->next;
-            head->next = prev;
-            prev = head;
-            head = next;
-        }
-        return prev;
-    }
-};
-
-
-// Recursive Approach
-// Time Complexity = O(N), Space Complexity = O(N)
-
-class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {        
-        if(head == NULL || head->next == NULL) return head;
+// 1st Approach
+// Using DFS
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+ class Solution {
+ public:
+     ListNode* reverseList(ListNode* head) {        
+         if(head == NULL || head->next == NULL) return head;
         
-        ListNode* node = reverseList(head->next);
-        head->next->next = head;
-        head->next = NULL;
-        return node;
+         ListNode* node = reverseList(head->next);
+         head->next->next = head;
+         head->next = NULL;
+         return node;
+     }
+ };
+
+
+// 2nd Approach
+// Using BFS
+// Time Complexity : O(N)
+// Space Complexity : O(N)
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head) return NULL;
+        
+        ListNode* prev = NULL;
+        
+        while(head && head->next){
+            ListNode* temp = head;
+            head = head->next;
+            temp->next = prev;
+            prev = temp;
+        }
+        
+        head->next = prev;        
+        return head;
     }
 };
