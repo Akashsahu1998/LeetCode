aa2e07d631ccc87f0971cd1e9d2dba6fa8b2a489
Akash Sahu <akkusahu559@gmail.com>
2022-05-03 22:47:19 +0530

    3 Approaches

--- Problems/Easy/26 - Remove Duplicates from Sorted Array.cpp
+++ Problems/Easy/26 - Remove Duplicates from Sorted Array.cpp
@@ -40,3 +40,27 @@ public:
         return index;
     }
 };
+
+
+
+// 3rd Approach
+// Time Complexity = O(N)
+// Space Complexity = O(1)
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        int i = 0, j = 0;
+        while(j < nums.size()){
+            if(nums[i] == nums[j]){
+                j++;
+            }
+            else {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        
+        return i+1;
+    }
+};
