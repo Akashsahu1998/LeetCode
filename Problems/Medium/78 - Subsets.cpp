403fe2f4709d82a04385025112f7b2cbae9300c1
Akash Sahu <akkusahu559@gmail.com>
2022-05-13 22:25:34 +0530

    2 Approaches

--- Problems/Medium/78 - Subsets.cpp
+++ Problems/Medium/78 - Subsets.cpp
@@ -4,20 +4,52 @@
 
 // Implementation
 
-// Time Complexity = O(N*(2^N)), Space Complexity = O(O(N*(2^N)))
+
+// 1st Approach
+// Recursive Approach
+// Time Complexity: O(2^N)
+// Space Complexity: O(2^N)
 class Solution {
+private: 
+    void generateSubsets(vector<int>& nums, vector<vector<int>> &res, vector<int> &temp, int index){
+        res.push_back(temp);
+        
+        for(int i = index; i < nums.size(); i++){
+            temp.push_back(nums[i]);
+            generateSubsets(nums, res, temp, i+1);   
+            temp.pop_back();
+        }        
+    }
+
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res;
-        res.push_back({});
-        for(int itr = 0; itr < nums.size(); itr++){
-            int resSize = res.size();
-            for(int jtr = 0; jtr < resSize; jtr++){
-                vector<int> temp = res[jtr];
-                temp.push_back(nums[itr]);
-                res.push_back(temp);
-            }
-        }
+        vector<int> temp;
+        generateSubsets(nums, res, temp, 0);
+        
         return res;
     }
 };
+
+
+// 2nd Approach
+// Iterative Approach
+// Time Complexity: O(N * 2^N)
+// Space Complexity: O(2^N)
+ class Solution {
+ public:
+     vector<vector<int>> subsets(vector<int>& nums) {
+         vector<vector<int>> res;
+         res.push_back({});
+        
+         for(int i = 0; i < nums.size(); i++){            
+             int resSize = res.size();            
+             for(int j = 0; j < resSize; j++){
+                 vector<int> temp = res[j];
+                 temp.push_back(nums[i]);
+                 res.push_back(temp);
+             }
+         }        
+         return res;
+     }
+ };
