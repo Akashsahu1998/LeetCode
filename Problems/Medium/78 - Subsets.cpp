af986482f47c7bce913b6e8f0305a482045a70ab
Akash Sahu <akkusahu559@gmail.com>
2022-05-14 17:11:31 +0530

    // 2nd Approach // Recursive Approach // Time Complexity: O(2^N) // Space Complexity: O(2^N) class Solution { private:     void generateSubsets(vector<int>& nums, vector<vector<int>> &res, vector<int> &temp, int index){         if(index == nums.size()){             res.push_back(temp);             return;         }
            // pick
            temp.push_back(nums[index]);
            generateSubsets(nums, res, temp, index+1);
            temp.pop_back();
    
            // not pick
            generateSubsets(nums, res, temp, index+1);

--- Problems/Medium/78 - Subsets.cpp
+++ Problems/Medium/78 - Subsets.cpp
@@ -31,8 +31,39 @@ public:
     }
 };
 
-
 // 2nd Approach
+// Recursive Approach
+// Time Complexity: O(2^N)
+// Space Complexity: O(2^N)
+class Solution {
+private: 
+    void generateSubsets(vector<int>& nums, vector<vector<int>> &res, vector<int> &temp, int index){
+        if(index == nums.size()){
+            res.push_back(temp);
+            return;
+        }
+        
+        // pick
+        temp.push_back(nums[index]);
+        generateSubsets(nums, res, temp, index+1);
+        temp.pop_back();
+        
+        // not pick
+        generateSubsets(nums, res, temp, index+1);
+    }
+
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> temp;
+        generateSubsets(nums, res, temp, 0);
+        
+        return res;
+    }
+};
+
+
+// 3rd Approach
 // Iterative Approach
 // Time Complexity: O(N * 2^N)
 // Space Complexity: O(2^N)
