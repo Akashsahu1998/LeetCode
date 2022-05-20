6a7566c3e35be19c12ffef75509b04c535d685dc
Akash Sahu <akkusahu559@gmail.com>
2022-05-21 00:20:23 +0530

    Using Binary Search, 2 Approaches

--- Problems/Medium/540 - Single Element in a Sorted Array.cpp
+++ Problems/Medium/540 - Single Element in a Sorted Array.cpp
@@ -5,7 +5,34 @@
 // Implementation
 
 // Using Binary Search
-// Time Complexity = O(log N), Space Complexity = O(1)
+// 1st Approach
+// Time Complexity = O(LogN)
+// Space Complexity = O(1)
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int left = 0, right = nums.size()-2; 
+       
+        while(left <= right){
+            int mid = left + (right-left)/2;
+           
+            if(mid%2 == 0 && nums[mid+1] == nums[mid] || 
+               mid%2 == 1 && nums[mid-1] == nums[mid]) {
+                left = mid+1;
+            }
+            else right = mid-1;
+        }
+        return nums[left];
+    }
+};
+
+
+// Using Binary Search
+// 2nd Approach
+// Time Complexity = O(LogN)
+// Space Complexity = O(1)
+
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
