0d7aadb5e53fd304fa92966087ec9c13a584311a
Akash Sahu <akkusahu559@gmail.com>
2022-05-21 22:30:32 +0530

    Using Recursion

--- Problems/Easy/94 - Binary Tree Inorder Traversal.cpp
+++ Problems/Easy/94 - Binary Tree Inorder Traversal.cpp
@@ -4,8 +4,10 @@
 
 // Implementation
 
-// 1st Approach in C++
-// Time Complexity = O(N), Space Complexity = O(H) where H is the height of the Binary Tree
+// Using Recursion
+// 1st Approach
+// Time Complexity = O(N)
+// Space Complexity = O(H), where H is the height of the Binary Tree
 
 class Solution {
 public:
@@ -16,6 +18,33 @@ public:
             res.push_back(root->val);
             inorderTraversal(root->right);    
         }
+        return res;
+    }
+};
+
+
+
+// Using Recursion
+// 2nd Approach
+// Time Complexity : O(N)
+// Space Complexity : O(N), where H is the height of the Binary Tree
+
+class Solution {
+private:
+    void solve(TreeNode* root, vector<int> &res){
+        if(!root) return;
+        
+        solve(root->left, res);
+        res.push_back(root->val);
+        solve(root->right, res);
+    }
+    
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        
+        solve(root, res);
+        
         return res;
     }
 };
