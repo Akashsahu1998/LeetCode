93f6b913cae507bbd4e1cae8552e8bf080b53b7d
Akash Sahu <akkusahu559@gmail.com>
2022-05-23 17:13:07 +0530

    2nd Approach, Recursive / DFS Approach

--- Problems/Medium/98 - Validate Binary Search Tree.cpp
+++ Problems/Medium/98 - Validate Binary Search Tree.cpp
@@ -34,3 +34,26 @@ public:
         return true;
     }
 };
+
+
+
+// 2nd Approach
+// Recursive / DFS Approach
+// Time Complexity = O(N), where N is the total nuumber of nodes in the Binary Tree
+// Space Complexity = O(N), In the case of skewed tree it will read all the n nodes in the Binary Tree
+// But, this approach is fine, but we can't rely over the max & min value, bcz the constraint can increase any time in future.
+class Solution {
+private:
+    bool solve(TreeNode* root, long long minValue, long long maxValue){
+        if(!root) return true;
+        
+        if(root->val <= minValue || root->val >= maxValue) return false;
+        
+        return solve(root->left, minValue, root->val) && solve(root->right, root->val, maxValue);
+    }
+
+public:
+    bool isValidBST(TreeNode* root) {
+        return solve(root, LLONG_MIN, LLONG_MAX);
+    }
+};
