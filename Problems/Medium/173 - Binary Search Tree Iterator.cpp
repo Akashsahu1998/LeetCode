1f27d2cbd4fd66906537042ad6e4d21a19f11d69
Akash Sahu <akkusahu559@gmail.com>
2022-05-24 18:54:01 +0530

    Using Stack

---
+++ Problems/Medium/173 - Binary Search Tree Iterator.cpp
@@ -0,0 +1,42 @@
+
+// Question) 173. Binary Search Tree Iterator
+
+
+// Implementation
+
+// Using Stack
+// Time Complexity = O(N)
+// Space Complexity = O(H)
+class BSTIterator {
+private:
+    stack<TreeNode*> st;
+
+public:
+    BSTIterator(TreeNode* root) {
+        pushAll(root);
+    }
+    
+    int next() {
+        TreeNode* node = st.top();
+        st.pop();
+        pushAll(node->right);
+        return node->val;
+    }
+    
+    bool hasNext() {
+        return !st.empty();
+    }
+    
+    void pushAll(TreeNode* root){
+        for(TreeNode* i = root; i != NULL; i = i->left){
+            st.push(i);
+        }
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
