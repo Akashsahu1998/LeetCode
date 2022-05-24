88a268f2a548b0c8e89556224cddc3433d9b6277
Akash Sahu <akkusahu559@gmail.com>
2022-05-24 08:42:58 +0530

    Iterative Approach

--- Problems/Easy/235 - Lowest Common Ancestor of a Binary Search Tree.cpp
+++ Problems/Easy/235 - Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -24,3 +24,29 @@ public:
         return root;
     }
 };
+
+
+
+// 2nd Approach
+// Iterative Approach
+// Time Complexity = O(H), Where H is the height of the BST
+// Space Complexity = O(1)
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root) return NULL;
+        
+        while(true){
+            int curValue = root->val;
+
+            // if possibility of p & q are going on left side
+            if(p->val < curValue && q->val < curValue) root = root->left;
+
+            // if possibility of p & q are going on right side
+            else if(p->val > curValue && q->val > curValue) root = root->right;
+            
+            else return root;
+        }
+        return root;        
+    }
+};
