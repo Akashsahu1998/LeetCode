54639e9f6615ecc2798b553bf3d7a8ea08721ce1
Akash Sahu <akkusahu559@gmail.com>
2022-05-25 09:14:29 +0530

    2nd Approach, Using Queue, Level Order Traversal

--- Problems/Hard/297 - Serialize and Deserialize Binary Tree.cpp
+++ Problems/Hard/297 - Serialize and Deserialize Binary Tree.cpp
@@ -40,4 +40,73 @@ public:
 };
 
 
+// 2nd Approach
+// Level Order Traversal
+// Using Queue
+// Time Complexity = O(N)
+// Space Complexity = O(N)
+class Codec {
+public:
 
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        string res;
+        
+        if(!root) return res;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            TreeNode* node = q.front();
+            q.pop();
+            
+            if(node) res.append(to_string(node->val) + ',');
+            else res.append("#,");
+            
+            if(node){
+                q.push(node->left);
+                q.push(node->right);
+            }
+        }
+        
+        return res;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        if(data.size() == 0) return NULL;
+        
+        stringstream s(data);
+        string str;
+        
+        getline(s,str,',');
+        TreeNode* root = new TreeNode(stoi(str));
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            TreeNode* node = q.front();
+            q.pop();
+            
+            getline(s,str,',');
+            if(str != "#"){
+                node->left = new TreeNode(stoi(str));
+                q.push(node->left);
+            }
+            
+            getline(s,str,',');
+            if(str != "#"){
+                node->right = new TreeNode(stoi(str));
+                q.push(node->right);
+            }
+        }
+        
+        return root;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
