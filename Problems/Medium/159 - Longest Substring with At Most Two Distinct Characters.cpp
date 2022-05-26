707bb94a0a84d87fac94c4e49371db521a43ae82
Akash Sahu <akkusahu559@gmail.com>
2022-05-26 09:43:58 +0530

    2nd Approach, Using Unordered Map

--- Problems/Medium/159 - Longest Substring with At Most Two Distinct Characters.cpp
+++ Problems/Medium/159 - Longest Substring with At Most Two Distinct Characters.cpp
@@ -5,7 +5,7 @@
 // Implementation
 
 // 1st Approach
-// Using Unordered Map
+// Using Unordered Map & Windows Sliding Technique
 // Time Complexity = O(N)
 // Space Complexity = O(N)
 
@@ -50,3 +50,45 @@ public:
         return maxLenAns;
     }
 };
+
+
+// 2nd Approach
+// Using Unordered Map & Windows Sliding Technique
+// Time Complexity = O(N)
+// Space Complexity = O(N)
+class Solution {
+public:
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+        int n = s.length();
+        if (n < 3) return n;
+
+        // sliding window left and right pointers
+        int left = 0, right = 0;
+        
+        // hashmap character -> its rightmost position
+        map<char, int> mp;
+
+        int maxLenAns = 2;
+
+        while (right < n) {
+            // when the sliding window contains less than 3 characters
+            mp[s[right]] = right;
+            right++;
+
+            // slidewindow contains 3 characters
+            if(mp.size() == 3){
+                int delIndex = INT_MAX;
+                for (pair<char, int> element : mp) {
+                    delIndex = min(delIndex, element.second);
+                }
+                // delete the leftmost character
+                mp.erase(s[delIndex]);
+                left = delIndex + 1;
+            }
+            maxLenAns = max(maxLenAns, right - left);
+        }
+        return maxLenAns;
+    }
+};
+
+
