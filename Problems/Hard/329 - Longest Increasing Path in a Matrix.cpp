93d8a41897136fdc814b6763912f8b6c5755d242
Akash Sahu <akkusahu559@gmail.com>
2022-05-28 16:28:33 +0530

    Using DFS & Memoization

--- Problems/Hard/329 - Longest Increasing Path in a Matrix.cpp
+++ Problems/Hard/329 - Longest Increasing Path in a Matrix.cpp
@@ -4,48 +4,49 @@
 
 // Implementation
 
-// DFS
-// Memoization Approach
-// Time Complexity = O(M*N), Space Complexity = O(M*N)
+// Using DFS & Memoization
+// Time Complexity = O(M * N)
+// Space Complexity = O(M * N)
 class Solution {
-public:
-    int direction[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-    
-    int dfs(vector<vector<int>>& matrix, int itr, int jtr, vector<vector<int>> &visited){
-        if(visited[itr][jtr] > 0) return visited[itr][jtr];
-        
-        int ans = 0;
-        for(int ktr = 0; ktr < 4; ktr++){
-            int newItr = itr + direction[ktr][0];
-            int newJtr = jtr + direction[ktr][1];
-            if(newItr >= 0 && newJtr >= 0 && newItr < matrix.size() && newJtr < matrix[0].size() &&
-               matrix[newItr][newJtr] > matrix[itr][jtr]){
-                ans = max(ans, dfs(matrix, newItr, newJtr, visited));
-            }
-        }
-        
-        visited[itr][jtr] = ans+1;
-        return ans+1;
+private:
+    bool isValid(vector<vector<int>> &matrix, int i, int j){
+        return (i >= 0 && j >= 0 && i < matrix.size() && j < matrix[i].size());
     }
     
+    int findIncreasingPath(vector<vector<int>> &cache, vector<vector<int>> &matrix, int i, int j){
+        if(cache[i][j]) return cache[i][j];
+        
+        // Top, left, bottom, right
+        int directions[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        
+        int ans = 0;
+        for(auto direction : directions){
+            int newI = i + direction[0];
+            int newJ = j + direction[1];
+            
+            if(isValid(matrix, newI, newJ) && matrix[i][j] < matrix[newI][newJ]){
+                ans = max(ans, findIncreasingPath(cache, matrix, newI, newJ));
+            }
+        }
+        
+        return cache[i][j] = ans+1;        
+    }
+
+public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int maxAns = 0, row = matrix.size(), col = matrix[0].size();        
+        int row = matrix.size();
+        if(!row) return 0;
         
-        vector<vector<int>> visited;        
-        for(int itr = 0; itr < row; itr++){
-            vector<int> temp;
-            for(int jtr = 0; jtr < col; jtr++){              
-                temp.push_back(0);
-            }
-            visited.push_back(temp);
-        }
+        int col = matrix[0].size(), maxPath = 0;
         
-        for(int itr = 0; itr < row; itr++){
-            for(int jtr = 0; jtr < col; jtr++){              
-                int ans = dfs(matrix, itr, jtr, visited);
-                maxAns = max(maxAns, ans);
+        vector<vector<int>> cache(row, vector<int>(col));
+                                
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                maxPath = max(maxPath, findIncreasingPath(cache, matrix, i, j));
             }
         }
-        return maxAns;
+        
+        return maxPath;
     }
 };
