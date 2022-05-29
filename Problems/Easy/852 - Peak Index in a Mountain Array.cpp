389cf620e0213cd0b299d1be307f8bfdbe06ca15
Akash Sahu <akkusahu559@gmail.com>
2022-05-29 17:40:26 +0530

    3rd Approach, Using Binary Search

--- Problems/Easy/852 - Peak Index in a Mountain Array.cpp
+++ Problems/Easy/852 - Peak Index in a Mountain Array.cpp
@@ -35,6 +35,30 @@ public:
 };
 
 
+// 3rd Approach
+// Using Binary Search
+// Time Complexity : O(logN)
+// Space Complexity : O(1)
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int low = 0, high = arr.size()-1;
+        
+        while(low < high){
+            int mid = low + (high-low)/2;
+            
+            if(arr[mid] < arr[mid+1]){
+                low = mid+1;
+            }
+            else high = mid;
+        }
+        
+        return low;
+    }
+};
+
+
 
 // Java Solution
 // Time Complexity = O(N)
