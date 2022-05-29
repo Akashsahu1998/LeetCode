297e723725eb4219d9a854a66cc3f12f6fbf002b
Akash Sahu <akkusahu559@gmail.com>
2022-05-29 17:39:55 +0530

    2nd Approach, Linear Search

--- Problems/Easy/852 - Peak Index in a Mountain Array.cpp
+++ Problems/Easy/852 - Peak Index in a Mountain Array.cpp
@@ -4,16 +4,41 @@
 
 // Implementation
 
-// 1st Approach in C++
-// Time Complexity = O(N), Space Complexity = O(1)
+// 1st Approach
+// Using Distance Function in C++
+// Time Complexity : O(N)
+// Space Complexity : O(1)
 
 int peakIndexInMountainArray(vector<int>& arr) {
     return distance(arr.begin(), max_element(arr.begin(), arr.end()));
 }
 
+// 2nd Approach
+// Using Linear Search
+// Time Complexity : O(N)
+// Space Complexity : O(1)
 
-// 2nd Approach in Java
-// Time Complexity = O(N), Space Complexity = O(1)
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int peakIndex = 0, maxValue = arr[0];
+        
+        for(int i = 0; i < arr.size(); i++){
+            if(maxValue < arr[i]){
+                maxValue = arr[i];
+                peakIndex = i;
+            }
+        }
+        
+        return peakIndex;
+    }
+};
+
+
+
+// Java Solution
+// Time Complexity = O(N)
+// Space Complexity = O(1)
 
 public int peakIndexInMountainArray(int[] arr) {
     int highestIndex = 0, maxElement = arr[0];
