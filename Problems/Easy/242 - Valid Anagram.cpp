50369cd0b097eec3d2e09b33b97c118230f971d0
Akash Sahu <akkusahu559@gmail.com>
2022-05-29 16:03:33 +0530

    Follow Up Que, Using Unordered Map

--- Problems/Easy/242 - Valid Anagram.cpp
+++ Problems/Easy/242 - Valid Anagram.cpp
@@ -27,3 +27,29 @@ public:
         return true;
     }
 };
+
+
+// ***** Follow Up Que *****
+// // Using Unordered Map
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        
+        unordered_map<char, int> freq;
+        
+        for(int i = 0; i < s.size(); i++){
+            freq[s[i]]++;
+            freq[t[i]]--;
+        }
+        
+        for(auto f : freq){
+            if(f.second != 0) return false;
+        }
+        
+        return true;
+    }
+};
