59baa43cc23642e79c287e133ed0623129eacddf
Akash Sahu <akkusahu559@gmail.com>
2022-06-04 00:05:08 +0530

    Using Disjoin Set Union and DFS, 2 Approaches

--- Problems/Medium/323 - Number of Connected Components in an Undirected Graph.cpp
+++ Problems/Medium/323 - Number of Connected Components in an Undirected Graph.cpp
@@ -4,6 +4,49 @@
 
 // Implementation
 
+// 1st Approach
+// Using DFS
+// Time Complexity = O(V + E)
+// Space Complexity = O(V + E)
+// where V is number of vertices and E is number of edges.
+
+class Solution {
+private:
+    void dfs(vector<int> graph[], vector<int> &visited, int node){
+        visited[node] = 1;
+        
+        for(auto adjacentNode : graph[node]){
+            if(!visited[adjacentNode]){
+                dfs(graph, visited, adjacentNode);
+            }
+        }
+    }
+    
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        
+        vector<int> visited(n, 0);
+        vector<int> graph[n];
+        
+        for(int i = 0; i < edges.size(); i++){
+            graph[edges[i][0]].push_back(edges[i][1]);
+            graph[edges[i][1]].push_back(edges[i][0]);
+        }
+        
+        int countComponents = 0;
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                dfs(graph, visited, i);
+                countComponents++;
+            }
+        }
+        
+        return countComponents;
+    }
+};
+
+
+// 2nd Approach
 // Using Disjoint Set Union
 // Time Complexity = O(E * a(N)), where a is the Inverse Ackermann Function and N will never go higher than 4 in the universe.
 // Space Complexity = O(V)
