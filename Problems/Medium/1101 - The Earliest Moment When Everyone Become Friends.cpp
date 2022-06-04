446101405097d9f7a26b0f97bfeab3f46bedfd3a
Akash Sahu <akkusahu559@gmail.com>
2022-06-04 13:42:48 +0530

    Using Disjoint Set Union

--- Problems/Medium/1101 - The Earliest Moment When Everyone Become Friends.cpp
+++ Problems/Medium/1101 - The Earliest Moment When Everyone Become Friends.cpp
@@ -8,7 +8,8 @@
 // Time Complexity = O(N + MlogM + M*a(M))
 // First of all, we sort the logs in the order of timestamp. The time complexity of sorting is O(MlogM).
 // Then we created a Union-Find data structure, which takes O(N) time to initialize the parent array.
-// Then we iterate through the sorted logs which are total M. At each iteration, we invoke the union(a, b) function which tooks a(M) Time. so the time complexity is O(M*a(M)), where a is the Inverse Ackermann Function and N will never go higher than 4 in the universe.
+// Then we iterate through the sorted logs which are total M. At each iteration, we invoke the union(a, b) function which tooks a(M) Time. so the time complexity is O(M*a(M)),
+// where a is the Inverse Ackermann Function and M will never go higher than 4 in the universe.
 
 // Space Complexity = O(N)
 // where M is the number of logs and N is the number of people
