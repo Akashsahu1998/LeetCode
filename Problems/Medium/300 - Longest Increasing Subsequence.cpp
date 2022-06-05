be9b23d2ba6bab57d6517b160897ef0db514e15c
Akash Sahu <akkusahu559@gmail.com>
2022-06-05 23:28:22 +0530

    Naive Approach

--- Problems/Medium/300 - Longest Increasing Subsequence.cpp
+++ Problems/Medium/300 - Longest Increasing Subsequence.cpp
@@ -5,37 +5,29 @@
 // Implementation
 
 
-// Naive Approach using DP
-// Time Complexity = O(N^2), Space Complexity = O(N)
+// Naive Approach
+// Using DP
+// Time Complexity : O(N^2)
+// Space Complexity : O(N)
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> dpArr(nums.size());
-        fill(dpArr.begin(), dpArr.end(), 1);
-        int res = 1;
-        for(int itr = 1; itr < nums.size(); itr++){
-            for(int jtr = 0; jtr < itr; jtr++){
-                if(nums[jtr] < nums[itr]) dpArr[itr] = max(dpArr[itr], dpArr[jtr]+1);
+        
+        // creating dp with value 1 in all index, bcz all element will have their own size as 1 atleast
+        vector<int> dp(nums.size(), 1);
+        
+        // intializing with 1 bcz atleast 1 length it will have
+        int longestLength = 1;
+        for(int i = 1; i < nums.size(); i++){
+            for(int j = 0; j < i; j++){
+                if(nums[i] > nums[j]){
+                    dp[i] = max(dp[i], dp[j]+1);
+                    longestLength = max(longestLength, dp[i]);
+                }
             }
-            res = max(res, dpArr[itr]);
-        }        
-        return res;
-    }
-};
-
-
-// Efficient Approach using binary search
-// Time Complexity = O(N log(N)), Space Complexity = O(N)
-class Solution {
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        vector<int> subsequence;
-        for(int itr = 0; itr < nums.size(); itr++){
-            auto index = lower_bound(subsequence.begin(), subsequence.end(), nums[itr]);
-            if(index == subsequence.end()) subsequence.push_back(nums[itr]);
-            else *index = nums[itr];
         }
-        return subsequence.size();
+        
+        return longestLength;
     }
 };
-
