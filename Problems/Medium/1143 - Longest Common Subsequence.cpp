4a0ed215772f60abf85401bef9daeb6972d257cc
Akash Sahu <akkusahu559@gmail.com>
2022-06-06 22:54:45 +0530

    3rd Approach, Iterative DP Approach

--- Problems/Medium/1143 - Longest Common Subsequence.cpp
+++ Problems/Medium/1143 - Longest Common Subsequence.cpp
@@ -56,3 +56,39 @@ public:
         return dfs(text1, text2, m-1, n-1, dp);
     }
 };
+
+
+// 3rd Approach
+// Iterative DP Approach
+// Time Complexity : O(M.N)
+// Space Complexity : O(M.N)
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size(), n = text2.size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1));
+        
+        // filling row first line
+        for(int i = 0; i <= m; i++){
+            dp[i][0] = 0;
+        }
+        
+        // filling column first line
+        for(int i = 0; i <= n; i++){
+            dp[0][i] = 0;
+        }
+        
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(text1[i-1] == text2[j-1]){
+                    dp[i][j] = 1 + max(dp[i][j], dp[i-1][j-1]);
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+};
