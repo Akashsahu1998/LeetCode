cec4f253db31f624ff8136eb31b42b41cc7ad5fa
Akash Sahu <akkusahu559@gmail.com>
2022-06-06 22:54:16 +0530

    2nd Approach, Recursive DP Approach

--- Problems/Medium/1143 - Longest Common Subsequence.cpp
+++ Problems/Medium/1143 - Longest Common Subsequence.cpp
@@ -28,3 +28,31 @@ public:
         return dfs(text1, text2, m-1, n-1);
     }
 };
+
+
+
+// 2nd Approach
+// Recursive DP Approach
+// Time Complexity : O(M.N)
+// Space Complexity : O(M.N + M.N) => O(M.N), bcz of stack trace
+class Solution {
+private:
+    int dfs(string &text1, string &text2, int index1, int index2, vector<vector<int>> &dp){
+        if(index1 < 0 || index2 < 0) return 0;
+        
+        if(dp[index1][index2] != -1) return dp[index1][index2];
+        
+        if(text1[index1] == text2[index2]){
+            return dp[index1][index2] = 1 + dfs(text1, text2, index1-1, index2-1, dp);
+        }
+        
+        return dp[index1][index2] = max(dfs(text1, text2, index1-1, index2, dp), dfs(text1, text2, index1, index2-1, dp));
+    }
+    
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size(), n = text2.size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return dfs(text1, text2, m-1, n-1, dp);
+    }
+};
