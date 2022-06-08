491601a47622966513bcb50ef5dae518735bda73
Akash Sahu <akkusahu559@gmail.com>
2022-06-08 23:30:02 +0530

    1st Approach, Recursive Approach

--- Problems/Medium/62 - Unique Paths.cpp
+++ Problems/Medium/62 - Unique Paths.cpp
@@ -4,22 +4,26 @@
 
 // Implementation
 
-// Using DP
-// Time Complexity: O(N*M)
-// Space Complexity: O(N*M)
+// 1st Approach
+// Recursive Approach
+// Will give TLE
+// Time Complexity : O(2!)
+// Space Complexity : O(2!)
 
 class Solution {
 private:
-    int solve(int m, int n, int left, int right, vector<vector<int>> &dp){
-        if(left >= m || right >= n) return 0;        
-        if(left == m-1 && right == n-1) return 1;
-        if(dp[left][right]) return dp[left][right];
-        return dp[left][right] = (solve(m, n, left+1, right, dp) + solve(m, n, left, right+1, dp));
+    int solve(int m, int n, int left, int up){
+        if(left == -1 || up == -1) return 0;
+        if(left == 0 && up == 0) return 1;
+        
+        int leftSide = solve(m, n, left-1, up);
+        int upSide = solve(m, n, left, up-1);
+        
+        return leftSide + upSide;
     }
     
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n));
-        return solve(m, n, 0, 0, dp);
+        return solve(m, n, m-1, n-1);
     }
 };
