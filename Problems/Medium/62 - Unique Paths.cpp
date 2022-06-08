a738b8e6d04a92713e64d76778c0154c5dcdfa69
Akash Sahu <akkusahu559@gmail.com>
2022-06-08 23:30:26 +0530

    2nd Approach, Memoization Approach

--- Problems/Medium/62 - Unique Paths.cpp
+++ Problems/Medium/62 - Unique Paths.cpp
@@ -27,3 +27,30 @@ public:
         return solve(m, n, m-1, n-1);
     }
 };
+
+
+// 2nd Approach
+// Memoization Approach
+// Time Complexity : O(M * N)
+// Space Complexity : O((M * N) + (M * N)) => O(M * N), bcz of recursion stack trace
+
+class Solution {
+private:
+    int solve(int m, int n, int left, int up, vector<vector<int>> &memo){
+        if(left == -1 || up == -1) return 0;
+        if(left == 0 && up == 0) return 1;
+        
+        if(memo[left][up] != -1) return memo[left][up];
+        
+        int leftSide = solve(m, n, left-1, up, memo);
+        int upSide = solve(m, n, left, up-1, memo);
+        
+        return memo[left][up] = leftSide + upSide;
+    }
+    
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> memo(m, vector<int> (n, -1));
+        return solve(m, n, m-1, n-1, memo);
+    }
+};
