425e1a3d7f314522f635d05996134826a5ce68e2
Akash Sahu <akkusahu559@gmail.com>
2022-06-11 23:12:33 +0530

    Using DP / Tabulation, Gap & Cut strategy

--- Problems/Hard/312 - Burst Balloons.cpp
+++ Problems/Hard/312 - Burst Balloons.cpp
@@ -4,38 +4,43 @@
 
 // Implementation
 
-// Tabulation
-// Using DP
+// Using DP / Tabulation
 // Gap & Cut strategy
-// Time Complexity = O(N^3), Space Complexity = O(N^2)
+// Time Complexity = O(N^3)
+// Space Complexity = O(N^2)
 class Solution {
 public:
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
+        
         vector<vector<int>> dp(n, vector<int>(n));
         
         for(int gap = 0; gap < n; gap++){
-            for(int itr = 0, jtr = gap; jtr < n; itr++, jtr++){
+            for(int i = 0, j = gap; j < n; i++, j++){
                 
-                // to hold the maximum possible value in the gap from itr to jtr
-                int maxValue = INT_MIN;
+                int maxBurstValue = INT_MIN;
                 
-                // running loop in the gap of itr to jtr
-                for(int ktr = itr; ktr <= jtr; ktr++){
-                    int left = (itr == ktr) ? 0 : dp[itr][ktr-1];
-                    int right = (jtr == ktr) ? 0 : dp[ktr+1][jtr];
-                    int val = ((itr == 0) ? 1 : nums[itr-1]) * (nums[ktr]) * ((jtr == n-1) ? 1 : nums[jtr+1]);
+                for(int k = i; k <= j; k++){
+                    int left = (k == i) ? 0 : dp[i][k-1];
+                    int right = (k == j) ? 0 : dp[k+1][j];
                     
-                    int total = left + right + val;
-                    maxValue = max(maxValue, total);
+                    // int multiplyValue = ((i == 0) ? 1 : nums[i-1]) * (nums[k]) * ((j == n-1) ? 1 : nums[j+1]);
+                    int multiplyValue = nums[k];
+                    if(i != 0){
+                        multiplyValue *= nums[i-1];
+                    }
+                    
+                    if(j != n-1){
+                        multiplyValue *= nums[j+1];
+                    }
+                    
+                    int total = left + right + multiplyValue;
+                    maxBurstValue = max(maxBurstValue, total);
                 }
-                
-                // storing the maxValue in dp, to resuse further
-                dp[itr][jtr] = maxValue;
+                dp[i][j] = maxBurstValue;
             }
         }
-                               
-        // returning the last index of the first row, it will gonna contain the highest possible value
-        return dp[0][n-1];                       
+        
+        return dp[0][n-1];
     }
 };
