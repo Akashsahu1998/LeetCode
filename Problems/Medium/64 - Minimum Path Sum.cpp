ddb4ca817463c943cbf6d51fffc6f631afada551
Akash Sahu <akkusahu559@gmail.com>
2022-06-12 16:22:28 +0530

    3rd Approach, Iterative DP Approach / Tabulation

--- Problems/Medium/64 - Minimum Path Sum.cpp
+++ Problems/Medium/64 - Minimum Path Sum.cpp
@@ -56,3 +56,47 @@ public:
         return dfs(grid, m-1, n-1, dp);
     }
 };
+
+
+
+
+// 3rd Approach
+// Iterative DP Approach / Tabulation
+// Time Complexity : O(M.N)
+// Space Complexity : O(M.N)
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n));
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = grid[i][j];
+                }                
+                else{
+                    int up = grid[i][j];
+                    if(i > 0){
+                        up += dp[i-1][j];
+                    }
+                    else{
+                        up += 1e9;
+                    }
+                    
+                    int left = grid[i][j];
+                    if(j > 0){
+                        left += dp[i][j-1];
+                    }
+                    else{
+                        left += 1e9;
+                    }
+
+                    dp[i][j] = min(up, left);
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
