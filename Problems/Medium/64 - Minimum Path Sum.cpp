3e51c4ec002b74c3f44cc5bd1f8aba23f939aefe
Akash Sahu <akkusahu559@gmail.com>
2022-06-12 16:21:38 +0530

    2nd Approach, Recursive DP Approach / Memoization

--- Problems/Medium/64 - Minimum Path Sum.cpp
+++ Problems/Medium/64 - Minimum Path Sum.cpp
@@ -27,3 +27,32 @@ public:
         return dfs(grid, m-1, n-1);
     }
 };
+
+
+
+// 2nd Approach
+// Recursive DP Approach / Memoization
+// Time Complexity : O(M.N)
+// Space Complexity : O(M.N + M.N) => O(M.N), bcz of stack trace
+
+class Solution {
+private:
+    int dfs(vector<vector<int>> &grid, int i, int j, vector<vector<int>> &dp){
+        if(i == 0 && j == 0) return grid[i][j];
+        if(i < 0 || j < 0) return 1e9;
+        
+        if(dp[i][j]) return dp[i][j];
+        
+        int up = grid[i][j] + dfs(grid, i-1, j, dp);
+        int left = grid[i][j] + dfs(grid, i, j-1, dp);
+        
+        return dp[i][j] = min(up, left);
+    }
+    
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n));
+        return dfs(grid, m-1, n-1, dp);
+    }
+};
