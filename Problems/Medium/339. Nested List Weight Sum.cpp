d9d868d1d469bc5f0d893a2f69d9f1852894bc48
Akash Sahu <akkusahu559@gmail.com>
2022-06-16 22:56:42 +0530

    BFS Approach, Using Queue

--- Problems/Medium/339. Nested List Weight Sum.cpp
+++ Problems/Medium/339. Nested List Weight Sum.cpp
@@ -4,9 +4,10 @@
 
 // Implementation
 
-// Using DFS
-// Time Complexity : O(N), Where is the total number of nested elements in the input list, i.e [ [[[[1]]]], 2] contains 44 nested lists and 22 nested integers, so N = 6 in this case
-// Space Complexity : O(D), Where D is the number of recursive calls made, in the worst case the S.C will becomd O(N), i.e ([[[[[[]]]]]])
+// DFS Approach
+// Using Recursion
+// Time Complexity : O(N), Where is the total number of nested elements in the input list, i.e [ [[[[1]]]], 2] contains 4 nested lists and 2 nested integers, so N = 6 in this case
+// Space Complexity : O(D), Where D is the number of recursive calls made, in the worst case the S.C will become O(N), i.e ([[[[[[]]]]]])
     
 class Solution {
 private:
@@ -28,5 +29,47 @@ private:
 public:
     int depthSum(vector<NestedInteger>& nestedList) {
         return dfs(nestedList, 1);
+    }
+};
+
+
+
+
+// BFS Approach
+// Using Queue
+// Time Complexity : O(N), Where is the total number of nested elements in the input list, i.e [ [[[[1]]]], 2] contains 4 nested lists and 2 nested integers, so N = 6 in this case
+// Space Complexity : O(N), in the worst case the S.C will become O(N), i.e [1, 2, 3, 4, 5]
+
+class Solution {
+public:
+    int depthSum(vector<NestedInteger>& nestedList) {
+        queue<NestedInteger> bfsQueue;
+        for(auto nested : nestedList){
+            bfsQueue.push(nested);
+        }
+        
+        int depth = 1, total = 0;
+        while(!bfsQueue.empty()){
+
+            int size = bfsQueue.size();
+            for(int i = 0; i < size; i++){
+                auto front = bfsQueue.front();
+                bfsQueue.pop();
+                
+                if(front.isInteger()){
+                    total += depth * front.getInteger();
+                }
+                else{
+                    for(auto nested : front.getList()){
+                        bfsQueue.push(nested);
+                    }
+                }
+            }
+            
+            // increase the depth
+            depth++;
+        }
+        
+        return total;
     }
 };
