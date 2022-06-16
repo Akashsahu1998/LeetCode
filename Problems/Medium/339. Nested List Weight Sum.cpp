15f92d750b71cd177ed0aadfa41477072db45038
Akash Sahu <akkusahu559@gmail.com>
2022-06-16 23:01:31 +0530

    2 Approaches

--- Problems/Medium/339. Nested List Weight Sum.cpp
+++ Problems/Medium/339. Nested List Weight Sum.cpp
@@ -6,8 +6,8 @@
 
 // DFS Approach
 // Using Recursion
-// Time Complexity : O(N), Where is the total number of nested elements in the input list, i.e [ [[[[1]]]], 2] contains 4 nested lists and 2 nested integers, so N = 6 in this case
-// Space Complexity : O(D), Where D is the number of recursive calls made, in the worst case the S.C will become O(N), i.e ([[[[[[]]]]]])
+// Time Complexity : O(N), Where N is the total number of nested elements in the input list, i.e [[[[[1]]]], 2] contains 4 nested lists and 2 nested integers, so N = 6 in this case
+// Space Complexity : O(D), Where D is the number of recursive calls made, in the worst case the S.C will become O(N), i.e [[[[[[]]]]]]
     
 class Solution {
 private:
@@ -37,7 +37,7 @@ public:
 
 // BFS Approach
 // Using Queue
-// Time Complexity : O(N), Where is the total number of nested elements in the input list, i.e [ [[[[1]]]], 2] contains 4 nested lists and 2 nested integers, so N = 6 in this case
+// Time Complexity : O(N), Where N is the total number of nested elements in the input list, i.e [[[[[1]]]], 2] contains 4 nested lists and 2 nested integers, so N = 6 in this case
 // Space Complexity : O(N), in the worst case the S.C will become O(N), i.e [1, 2, 3, 4, 5]
 
 class Solution {
