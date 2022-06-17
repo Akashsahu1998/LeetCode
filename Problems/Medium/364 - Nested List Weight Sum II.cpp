db6c92b933a521bbe0e33f60a85bc2118aaed2d2
Akash Sahu <akkusahu559@gmail.com>
2022-06-17 22:48:56 +0530

    2nd Approach, BFS Approach, Single Traversal

--- Problems/Medium/364 - Nested List Weight Sum II.cpp
+++ Problems/Medium/364 - Nested List Weight Sum II.cpp
@@ -4,7 +4,8 @@
 
 // Implementation
 
-// DFS Approach
+// 1st Approach
+// DFS Approach, Double Traversal
 // Using Recursion
 // Time Complexity : O(2N) => O(N), Where N is the total number of nested elements in the input list, i.e [[[[[1]]]], 2] contains 4 nested lists and 2 nested integers, so N = 6 in this case
 // Space Complexity : O(D), Where D is the number of recursive calls made, in the worst case the S.C will become O(N), i.e [[[[[[]]]]]]
@@ -38,5 +39,49 @@ public:
         int maxDepth = 1;
         findMaxDepth(nestedList, 1, maxDepth);
         return dfs(nestedList, 1, maxDepth);
+    }
+};
+
+
+// 2nd Approach
+// BFS Approach, Single Traversal
+// Using Queue
+// Time Complexity : O(N), Where N is the total number of nested elements in the input list, i.e [[[[[1]]]], 2] contains 4 nested lists and 2 nested integers, so N = 6 in this case
+// Space Complexity : O(N), in the worst case the S.C will become O(N), i.e [1, 2, 3, 4, 5]
+class Solution {
+public:
+    int depthSumInverse(vector<NestedInteger>& nestedList) {
+        int curDepth = 1, maxDepth = 1, sumOfElements = 0, sumOfProducts = 0;
+        
+        queue<NestedInteger> bfsQueue;
+        
+        for(auto nested : nestedList){
+            bfsQueue.push(nested);
+        }
+        
+        while(!bfsQueue.empty()){
+            int size = bfsQueue.size();
+            maxDepth = max(curDepth, maxDepth);
+            
+            for(int i = 0; i < size; i++){
+                NestedInteger front = bfsQueue.front();
+                bfsQueue.pop();
+                
+                if(front.isInteger()){
+                    sumOfElements += front.getInteger();
+                    sumOfProducts += front.getInteger() * curDepth;
+                }
+                else{
+                    for(auto nested : front.getList()){
+                        bfsQueue.push(nested);
+                    }
+                }
+            }
+            
+            curDepth++;
+        }
+        
+        // equation to get the results
+        return ((maxDepth + 1) * sumOfElements) - sumOfProducts;
     }
 };
