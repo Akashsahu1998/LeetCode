273e5588f2e6088f397687442efdb9f16f666cdd
Akash Sahu <akkusahu559@gmail.com>
2022-06-21 00:14:39 +0530

    Using Unordered Map

---
+++ Problems/Medium/244 - Shortest Word Distance II.cpp
@@ -0,0 +1,46 @@
+
+// Question) 244. Shortest Word Distance II
+
+
+// Implementation
+
+// Using Unordered Map
+// Time Complexity = O(N)
+// Space Complexity = O(N)
+
+class WordDistance {
+private:
+    unordered_map<string, vector<int>> mp;
+    
+public:
+    // O(N)
+    WordDistance(vector<string>& wordsDict) {
+        for(int i = 0; i < wordsDict.size(); i++){
+            mp[wordsDict[i]].push_back(i);
+        }
+    }
+    
+    // O(max(K, L)), Where K is the size of wordIndex1 and L is the size of wordIndex2
+    int shortest(string word1, string word2) {
+        vector<int> wordIndex1 = mp[word1];
+        vector<int> wordIndex2 = mp[word2];
+        
+        int index1 = 0, index2 = 0, minDistance = INT_MAX;
+        while(index1 < wordIndex1.size() && index2 < wordIndex2.size()){
+            minDistance = min(minDistance, abs(wordIndex1[index1] - wordIndex2[index2]));
+            
+            if(wordIndex1[index1] < wordIndex2[index2]){
+                index1++;
+            }
+            else index2++;
+        }
+        
+        return minDistance;
+    }
+};
+
+/**
+ * Your WordDistance object will be instantiated and called as such:
+ * WordDistance* obj = new WordDistance(wordsDict);
+ * int param_1 = obj->shortest(word1,word2);
+ */
