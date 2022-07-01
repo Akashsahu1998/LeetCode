23785af68622bc639c7f70f56323e55257cdc45e
Akash Sahu <akkusahu559@gmail.com>
2022-07-01 23:04:23 +0530

    Using Fibonacci Approach

---
+++ Problems/Easy/70 - Climbing Stairs.cpp
@@ -0,0 +1,27 @@
+
+// Question) 70. Climbing Stairs
+
+
+// Implementation
+
+// Using Fibonacci Approach
+// Time Complexity : O(N)
+// Time Complexity : O(1)
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 0) return n;
+        else if(n == 1) return n;
+        else if(n == 2) return n;
+        
+        int prev2 = 1, prev1 = 2;
+        for(int i = 3; i <= n; i++){
+            int temp = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = temp;
+        }
+        
+        return prev1;
+    }
+};
