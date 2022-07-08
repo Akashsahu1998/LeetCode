746291b4d9ce50881caab180d73aa3e8f9259b72
Akash Sahu <akkusahu559@gmail.com>
2022-07-08 23:55:54 +0530

    2nd Approach, Efficient Solution

--- Problems/Medium/238 - Product of Array Except Self.cpp
+++ Problems/Medium/238 - Product of Array Except Self.cpp
@@ -19,6 +19,7 @@ public:
         int n = nums.size();
         vector<int> leftSum(n), rightSum(n);
         
+        // Generating the leftSum & rightSum array
         for(int i = 0, j = n-1; i < n; i++, j--){
             if(i == 0 || j == n-1){
                 leftSum[i] = 1;
@@ -30,8 +31,51 @@ public:
             }
         }
         
+        // multiply the value of leftSum & rightSum
         for(int i = 0; i < n; i++){
             leftSum[i] = leftSum[i] * rightSum[i];
+        }
+        
+        // returning leftSum as a result
+        return leftSum;
+    }
+};
+
+
+
+// 2nd Approach
+// Efficient Solution
+// Using LeftSum array only and will use that array as a returning result array as well
+// Time Complexity = O(N)
+// Space Complexity = O(1), here leftSum is a part of output
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        
+        int n = nums.size(), rightSum = 1;
+        vector<int> leftSum(n);
+        
+        // Generating the leftSum array
+        for(int i = 0; i < n; i++){
+            if(i == 0){
+                leftSum[i] = 1;
+            }
+            else{
+                leftSum[i] = leftSum[i-1] * nums[i-1];
+            }
+        }
+        
+        // multiply the value of leftSum array & rightSum variable
+        for(int i = n-1; i >= 0; i--){
+            if(i == n-1){
+                leftSum[i] = rightSum * leftSum[i];
+            }
+            else{
+                leftSum[i] = rightSum * leftSum[i];
+            }
+            
+            rightSum = rightSum * nums[i];
         }
         
         // returning leftSum as a result
