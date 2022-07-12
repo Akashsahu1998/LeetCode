4466a78720f4f1bd3702fcb2400804232bb89b3a
Akash Sahu <akkusahu559@gmail.com>
2022-07-12 18:48:49 +0530

    3 Approaches

--- Problems/Easy/104 - Maximum Depth of Binary Tree.cpp
+++ Problems/Easy/104 - Maximum Depth of Binary Tree.cpp
@@ -4,31 +4,74 @@
 
 // Implementation
 
-// Recursive Approach 
-// Time Complexity = O(N), Space Complexity = O(H) where H is the height of the Binary Tree
+// 1st Approach
+// Using DFS
+// Time Complexity : O(N)
+// Space Complexity : O(N)
 
-int maxDepth(TreeNode* root) {
-    if(root == NULL) return 0;
-    return max(maxDepth(root->left), maxDepth(root->right))+1;
-}
+class Solution {
+private:
+    void dfs(TreeNode* root, int &maxDepth, int curDepth){
+        if(!root) return;
+        
+        maxDepth = max(maxDepth, curDepth);
+        dfs(root->left, maxDepth, curDepth+1);
+        dfs(root->right, maxDepth, curDepth+1);
+        return;
+    }
+    
+public:
+    int maxDepth(TreeNode* root) {
+        int maxDepth = 0;
+        dfs(root, maxDepth, 1);
+        return maxDepth;
+    }
+};
 
 
-// BFS Approach
-// Time Complexity = O(N), Space Complexity = O(W) where W is the width of the Binary Tree
-int maxDepth(TreeNode* root) {
-    if (!root) return 0;
-    int count = 0;        
-    queue<TreeNode*> q;
-    q.push(root);
-    while(!q.empty()){
-        count++;
-        int size = q.size();
-        for (int i = 0; i < size; i++){
-            TreeNode *cur = q.front();
-            q.pop();
-            if (cur->left) q.push(cur->left);                    
-            if (cur->right) q.push(cur->right);
+// 2nd Approach
+// Using DFS, without any extra variables
+// Time Complexity : O(N)
+// Space Complexity : O(N)
+
+class Solution {    
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
+
+
+// 3rd Approach
+// Using BFS, Using Queue
+// Time Complexity : O(N)
+// Space Complexity : O(N)
+
+class Solution {    
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        int maxDepth = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode* front = q.front();
+                q.pop();
+                
+                if(front->left){
+                    q.push(front->left);
+                }
+                if(front->right){
+                    q.push(front->right);
+                }
+            }
+            maxDepth++;
         }
-    }        
-    return count;
-}
+        
+        return maxDepth;
+    }
+};
