8ef47c4e30c1900f2fcd8cb3bfc7182ec7599f98
Akash Sahu <akkusahu559@gmail.com>
2022-07-14 23:43:41 +0530

    2 Approaches, Using slow & fast pointers

--- Problems/Easy/141 - Linked List Cycle.cpp
+++ Problems/Easy/141 - Linked List Cycle.cpp
@@ -4,8 +4,30 @@
 
 // Implementation
 
+// 1st Approach
 // Using slow & fast pointers
-// Time Complexity = O(N), Space Complexity = O(1)
+// Time Complexity = O(N)
+// Space Complexity = O(1)
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head) return false;
+        ListNode *slow = head, *fast = head->next;
+        while(fast && fast->next && slow != fast){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        return slow == fast;
+    }
+};
+
+
+// 2nd Approach
+// Using slow & fast pointers
+// Time Complexity = O(N)
+// Space Complexity = O(1)
 
 class Solution {
 public:
