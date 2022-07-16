7806ccec359933cbe8d3e3609235458881204a0a
Akash Sahu <akkusahu559@gmail.com>
2022-07-16 23:47:06 +0530

    Using Binary Search

--- Problems/Medium/33 - Search in Rotated Sorted Array.cpp
+++ Problems/Medium/33 - Search in Rotated Sorted Array.cpp
@@ -5,26 +5,28 @@
 // Implementation
 
 // Using Binary Search
-// Time Complexity = O(LogN), Space Complexity = O(1)
+// Time Complexity = O(LogN)
+// Space Complexity = O(1)
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0, right = nums.size()-1;
+        int low = 0, high = nums.size()-1;
         
-        while(left <= right){
-            int mid = left + (right-left)/2;
+        while(low <= high){
+            int mid = low + (high-low)/2;
             
-            if(nums[mid] == target) return mid;
+            if(target == nums[mid]) return mid;
             
-            if(nums[left] <= nums[mid]){
-                if(target >= nums[left] && target <= nums[mid]) right = mid-1;
-                else left = mid+1;
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target <= nums[mid]) high = mid-1;
+                else low = mid+1;
             }
             else{
-                if(target >= nums[mid] && target <= nums[right]) left = mid+1;
-                else right = mid-1;
+                if(nums[mid] <= target && target <= nums[high]) low = mid+1;
+                else high = mid-1;
             }
         }
+        
         return -1;
     }
 };
