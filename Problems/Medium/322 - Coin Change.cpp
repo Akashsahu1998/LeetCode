07c378e61d1add53c37c5d4223b0e16ba787fe92
Akash Sahu <akkusahu559@gmail.com>
2022-07-17 23:57:04 +0530

    2 Approaches

--- Problems/Medium/322 - Coin Change.cpp
+++ Problems/Medium/322 - Coin Change.cpp
@@ -5,29 +5,11 @@
 // Implementation
 
 // 1st Approach
-// Iterative solution using DP
-// Time Complexity = O(amount*coins.size()), Space Complexity = O(amount)
+// Recursive using DP
+// Time Complexity = O(amount*coins.size())
+// Space Complexity = O(amount)
 class Solution {
-public:
-    int coinChange(vector<int>& coins, int amount) {        
-        vector<int> dp(amount+1, amount+1);
-        dp[0] = 0;
-        for(int itr = 1; itr < dp.size(); itr++){
-            for(int jtr = 0; jtr < coins.size(); jtr++){
-                if(coins[jtr] > itr) continue;
-                dp[itr] = min(dp[itr - coins[jtr]]+1, dp[itr]);
-            }
-        }        
-        return (dp[amount] == amount+1) ? -1 : dp[amount];
-    }
-};
-
-
-// 2nd Approach
-// Recursive solution using DP
-// Time Complexity = O(amount*coins.size()), Space Complexity = O(amount)
-class Solution {
-public:
+public:    
     int findLeastCombination(vector<int>& coins, vector<int>& dp, int amount){
         if(amount < 0) return -1;
         if(amount == 0) return 0;
@@ -45,5 +27,25 @@ public:
     int coinChange(vector<int>& coins, int amount) {        
         vector<int> dp(amount+1);
         return findLeastCombination(coins, dp, amount);
+    }
+};
+
+
+// 2nd Approach
+// Iterative using DP
+// Time Complexity = O(amount*coins.size())
+// Space Complexity = O(amount)
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {        
+        vector<int> dp(amount+1, amount+1);
+        dp[0] = 0;
+        for(int itr = 1; itr < dp.size(); itr++){
+            for(int jtr = 0; jtr < coins.size(); jtr++){
+                if(coins[jtr] > itr) continue;
+                dp[itr] = min(dp[itr - coins[jtr]]+1, dp[itr]);
+            }
+        }        
+        return (dp[amount] == amount+1) ? -1 : dp[amount];
     }    
 };
