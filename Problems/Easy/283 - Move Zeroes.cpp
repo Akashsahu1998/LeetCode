80f94cb1501594f2bd8dcc85958610c99a9547d0
Akash Sahu <akkusahu559@gmail.com>
2022-07-30 22:13:01 +0530

    2 Approaches

--- Problems/Easy/283 - Move Zeroes.cpp
+++ Problems/Easy/283 - Move Zeroes.cpp
@@ -4,24 +4,44 @@
 
 // Implementation
 
-// 1st Approach in C++
+// 1st Approach
+// Using 1 pointers
 // Time Complexity = O(N), Space Complexity = O(1)
 
-void moveZeroes(vector<int>& nums) {    
-    for(int itr = 0, jtr = 0; jtr < nums.size(); itr++){
-        if(itr > nums.size()-1) nums[jtr++] = 0;
-        else if(nums[itr] != 0) nums[jtr++] = nums[itr];
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        for(int i = 0; j < nums.size(); i++){
+            if(i >= nums.size()){
+                nums[j++] = 0;
+            }
+            else if(nums[i] != 0){
+                nums[j++] = nums[i];
+            }
+        }
     }
-}
+};
 
 
-// 2nd Approach in Java
+// 2nd Approach
+// Using 2 pointers
 // Time Complexity = O(N), Space Complexity = O(1)
 
-public void moveZeroes(int[] nums) {
-    int jtr = 0;
-    for(int itr = 0; jtr < nums.length; itr++){
-        if(itr > nums.length-1) nums[jtr++] = 0;
-        else if(nums[itr] != 0) nums[jtr++] = nums[itr];
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0, j = 1;
+        while(j < nums.size()){
+            if(nums[j] != 0 && nums[i] == 0){
+                swap(nums[i], nums[j]);
+                i++;
+            }
+            j++;
+            
+            if(nums[i] != 0){
+                i++;
+            }
+        }
     }
-}
+};
