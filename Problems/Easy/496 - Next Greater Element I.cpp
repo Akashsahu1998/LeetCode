0c6141dc9f4b5faceb88418ea73ade9ee9c7efe9
Akash Sahu <akkusahu559@gmail.com>
2022-08-17 22:16:05 +0530

    2 Approaches

--- Problems/Easy/496 - Next Greater Element I.cpp
+++ Problems/Easy/496 - Next Greater Element I.cpp
@@ -6,34 +6,6 @@
 
 // 1st Approach
 // Using Map and Stack
-// Traversing from back
-// Time Complexity = O(N), Space Complexity = O(N)
-class Solution {
-public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> s;
-        unordered_map<int, int> mp;
-        vector<int> res;
-        
-        for(int itr = nums2.size()-1; itr >= 0; itr--){
-            while(!s.empty() && s.top() < nums2[itr]){
-                s.pop();
-            }
-            if(s.empty()) mp[nums2[itr]] = -1;
-            else mp[nums2[itr]] = s.top();
-            s.push(nums2[itr]);
-        }
-        
-        for(int itr = 0; itr < nums1.size(); itr++){
-            res.push_back(mp[nums1[itr]]);
-        }
-        return res;
-    }
-};
-
-
-// 2nd Approach
-// Using Map and Stack
 // Traversing from start
 // Time Complexity = O(N), Space Complexity = O(N)
 class Solution {
@@ -55,6 +27,34 @@ public:
             mp[nums1[itr]] ? res.push_back(mp[nums1[itr]]) : res.push_back(-1);
         }
         
+        return res;
+    }
+};
+
+
+// 2nd Approach
+// Using Map and Stack
+// Traversing from back
+// Time Complexity = O(N), Space Complexity = O(N)
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> s;
+        unordered_map<int, int> mp;
+        vector<int> res;
+        
+        for(int itr = nums2.size()-1; itr >= 0; itr--){
+            while(!s.empty() && s.top() < nums2[itr]){
+                s.pop();
+            }
+            if(s.empty()) mp[nums2[itr]] = -1;
+            else mp[nums2[itr]] = s.top();
+            s.push(nums2[itr]);
+        }
+        
+        for(int itr = 0; itr < nums1.size(); itr++){
+            res.push_back(mp[nums1[itr]]);
+        }
         return res;
     }
 };
