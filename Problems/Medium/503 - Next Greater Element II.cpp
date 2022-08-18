f3c409b7072438a74477a680dcf763f4114ff815
Akash Sahu <akkusahu559@gmail.com>
2022-08-18 23:07:33 +0530

    2 Approaches

--- Problems/Medium/503 - Next Greater Element II.cpp
+++ Problems/Medium/503 - Next Greater Element II.cpp
@@ -4,19 +4,73 @@
 
 // Implementation
 
+// 1st Approach
 // Using Stack
-// Time Complexity = O(2N), Space Complexity = O(N)
+// Only 2 traversal of array
+// Time Complexity = O(N)
+// Space Complexity = O(N)
+
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        int n = nums.size();
-        stack<int> s;        
-        vector<int> res(n, -1);
-        for(int itr = 2*n-1; itr >= 0; itr--){
-            while(!s.empty() && s.top() <= nums[itr%n]) s.pop();
-            if(!s.empty()) res[itr%n] = s.top();
-            s.push(nums[itr%n]);
+        stack<int> st;
+        vector<int> res(nums.size(), -1);
+        
+        for(int i = nums.size()-1; i >= 0; i--){
+            while(!st.empty() && st.top() <= nums[i]){
+                st.pop();
+            }
+            st.push(nums[i]);
         }
+        
+        for(int i = nums.size()-1; i >= 0; i--){
+            while(!st.empty() && st.top() <= nums[i]){
+                st.pop();
+            }
+            
+            if(st.empty()){
+                res[i] = -1;
+            }
+            else{
+                res[i] = st.top();
+            }
+            
+            st.push(nums[i]);
+        }
+        
+        return res;
+    }
+};
+
+
+// 2nd Approach
+// Using Stack
+// Only 2 traversal of array
+// Time Complexity = O(N)
+// Space Complexity = O(N)
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st;
+        vector<int> res(nums.size(), -1);
+        int n = nums.size();
+        
+        for(int i = n*2-1; i >= 0; i--){
+            while(!st.empty() && st.top() <= nums[i%n]){
+                st.pop();
+            }
+            
+            if(st.empty()){
+                res[i%n] = -1;
+            }
+            else{
+                res[i%n] = st.top();
+            }
+            
+            st.push(nums[i%n]);
+        }
+        
         return res;
     }
 };
