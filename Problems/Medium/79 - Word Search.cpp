aaa1a18c270a51ee68a0ec5d0893dadddb83c3af
Akash Sahu <akkusahu559@gmail.com>
2022-09-15 00:15:59 +0530

    Using DFS + Backtracking

--- Problems/Medium/79 - Word Search.cpp
+++ Problems/Medium/79 - Word Search.cpp
@@ -5,38 +5,37 @@
 // Implementation
 
 // Using DFS + Backtracking
-// Time Complexity = O(ROW * COL * 3)
+// Time Complexity = O(ROW * COL * L), where L is the length of the word to be matched
 // Space Complexity = O(L), where L is the length of the word to be matched
 class Solution {
-private:    
-    bool dfs(vector<vector<char>>& board, string word, int itr, int jtr, int index){
+private:
+    bool dfs(vector<vector<char>>& board, string word, int row, int col, int index){
         if(index == word.size()) return true;
         
-        if(itr >= 0 && itr < board.size() && jtr >= 0 && jtr < board[0].size() && board[itr][jtr] == word[index]){
+        // checking whether row & col is inside the boundary or not
+        if(row >= 0 && col >= 0 && row < board.size() && col < board[0].size() && board[row][col] == word[index]){
             
-            char backup = board[itr][jtr];
-            board[itr][jtr] = '*';
-            
-            int result = (dfs(board, word, itr-1, jtr, index+1)) ||
-                         (dfs(board, word, itr, jtr-1, index+1)) ||
-                         (dfs(board, word, itr+1, jtr, index+1)) ||
-                         (dfs(board, word, itr, jtr+1, index+1));
-            
-            // backtrack
-            board[itr][jtr] = backup;
-            
+            // backtracking
+            // creating the backup of current char & putting * into it, bcz we don't want to check it again when we proceed further to find another char of word
+            char backup = board[row][col];
+            board[row][col] = '*';
+            bool result = (dfs(board, word, row-1, col, index+1) ||
+                        dfs(board, word, row, col-1, index+1) ||
+                        dfs(board, word, row+1, col, index+1) ||
+                        dfs(board, word, row, col+1, index+1));
+                
+            board[row][col] = backup;
             return result;
         }
+        
         return false;
     }
-
+    
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        for(int itr = 0; itr < board.size(); itr++){
-            for(int jtr = 0; jtr < board[0].size(); jtr++){
-                if(board[itr][jtr] == word[0]){
-                    if(dfs(board, word, itr, jtr, 0)) return true;
-                }
+        for(int row = 0; row < board.size(); row++){
+            for(int col = 0; col < board[row].size(); col++){
+                if(dfs(board, word, row, col, 0)) return true;
             }
         }
         
