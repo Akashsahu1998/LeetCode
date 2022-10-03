ee4eba14a7db443ad5ec155bc3922739cbd12684
Akash Sahu <akkusahu559@gmail.com>
2022-10-03 22:33:55 +0530

    Using BFS/Queue

---
+++ Problems/Medium/1161. Maximum Level Sum of a Binary Tree.cpp
@@ -0,0 +1,39 @@
+
+// Question) 1161. Maximum Level Sum of a Binary Tree
+
+
+// Implementation
+
+
+// Using BFS/Queue
+// Time Complexity = O(N)
+// Space Complexity = O(N)
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int curLevel = 1, maxLevel = 1, maxSum = root->val;
+        
+        while(!q.empty()){
+            int size = q.size(), sum = 0;
+            for(int i = 0; i < size; i++){
+                TreeNode* temp = q.front();
+                q.pop();
+                
+                sum += temp->val;
+                
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+            }
+            
+            if(sum > maxSum){
+                maxLevel = curLevel;
+                maxSum = sum;
+            }
+            curLevel++;
+        }
+        
+        return maxLevel;
+    }
+};
