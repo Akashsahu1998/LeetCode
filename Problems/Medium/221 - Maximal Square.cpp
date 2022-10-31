eacd9eca896a3f2bc1f5d96a16e6d928b4372b00
Akash Sahu <akkusahu559@gmail.com>
2022-10-31 13:48:27 +0530

    Using DP, Comments Added

--- Problems/Medium/221 - Maximal Square.cpp
+++ Problems/Medium/221 - Maximal Square.cpp
@@ -7,19 +7,24 @@
 // 1st Approach
 // Using DP
 // Time Complexity = O(M*N)
-// Space Complexity = O(M*N)
-
+// Space Complexity = O(M*N), we are using extra space of 2-D vector
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
         
         int rows = matrix.size(), cols = matrix[0].size(), largestSquare = 0;
+        
+        // taking +1 more size, bcz to avoid corner cases
         vector<vector<int>> dp(rows+1, vector<int>(cols+1, 0));
         
         for(int i = 1; i <= rows; i++){
             for(int j = 1; j <= cols; j++){
-                if(matrix[i-1][j-1] == '1'){                    
+                if(matrix[i-1][j-1] == '1'){
+                    // here we have to check three places to calculate the square
+                    // up, left, left-up
                     dp[i][j] = 1 + min(dp[i-1][j], min(dp[i-1][j-1], dp[i][j-1]));
+                    
+                    // keep track maximal square
                     largestSquare = max(largestSquare, dp[i][j]);
                 }
             }   
