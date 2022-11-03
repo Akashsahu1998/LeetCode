059ea4072302cc36aaed19c30e293c6860debd5f
Akash Sahu <akkusahu559@gmail.com>
2022-11-03 23:35:52 +0530

    Iterative Solution

--- Problems/Medium/430 - Flatten a Multilevel Doubly Linked List.cpp
+++ Problems/Medium/430 - Flatten a Multilevel Doubly Linked List.cpp
@@ -4,34 +4,53 @@
 
 // Implementation
 
-// Time Complexity = O(N), Space Complexity = O(1)
+// Iterative Solution
+// Time Complexity = O(N),
+// Space Complexity = O(1), no Space used, we just created few pointers, but they are pointing, they are not storing anything 
 class Solution {
 public:
     Node* flatten(Node* head) {
-        Node *ptr = head, *cur = NULL, *storedNode = NULL;
+        Node *ptr = head, *curr = NULL, *storedNode = NULL;
         
+        // ptr will iterate through each element
         while(ptr){
+            
+            // if child is there, then go inside it.
             if(ptr->child){
-                // storing the ptr->next, and pointing the ptr next to child
+                // store the node
                 storedNode = ptr->next;
+                
+                // assign the child to its next
                 ptr->next = ptr->child;
-                ptr->next->prev = ptr;                
+                
+                // assign the prev to itself
+                ptr->next->prev = ptr;
+                
+                // put NULL into child, bcz not its ended
                 ptr->child = NULL;
                 
-                // running till the end of the child list
-                cur = ptr->next;
-                while(cur->next){
-                    cur = cur->next;
+                // take curr from ptr->next
+                curr = ptr->next;
+                
+                // run till the last, bcz we need to assign storedNode into cur last node
+                while(curr->next){
+                    curr = curr->next;
                 }
                 
-                // connect the storedNode at the last of the child list
-                cur->next = storedNode;
-                if(cur->next){
-                    cur->next->prev = cur;
+                // assigning the storeNode into the curr last node
+                curr->next = storedNode;
+                
+                // if next is there, it means storedNode was there, then assign the curr(itself) as the prev node
+                if(curr->next){
+                    curr->next->prev = curr;
                 }
             }
+            
+            // always move ptr by 1 step
             ptr = ptr->next;
         }
+        
+        // return head as result
         return head;
     }
 };
