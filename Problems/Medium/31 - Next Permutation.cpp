37543a3a410e24292ea5c7e29820457679bd888c
Akash Sahu <akkusahu559@gmail.com>
2022-11-06 11:52:10 +0530

    Using 2 Pointers

--- Problems/Medium/31 - Next Permutation.cpp
+++ Problems/Medium/31 - Next Permutation.cpp
@@ -4,18 +4,30 @@
 
 // Implementation
 
+// Reference : https://www.youtube.com/watch?v=LuLCLgMElus&t=602s
+
+// Using 2 Pointers
 // Time Complexity = O(N)
 // Space Complexity = O(1)
+
 class Solution {
 public:
     void nextPermutation(vector<int>& arr) {
+
+        // finding first decreasing element
         int itr = arr.size()-2;
         while(itr >= 0 && arr[itr] >= arr[itr+1]) itr--;
         if(itr >= 0){
+
+            // check from back if any element is greater than arr[itr]
             int jtr = arr.size()-1;
             while(arr[itr] >= arr[jtr]) jtr--;
+
+            // swap both itr & jtr index values
             swap(arr[itr], arr[jtr]);
         }
+
+        // reverse the array from itr+1 index
         reverse(arr.begin()+itr+1, arr.end());
     }
 };
