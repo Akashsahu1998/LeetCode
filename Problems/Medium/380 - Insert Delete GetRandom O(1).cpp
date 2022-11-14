a0cc91269aac9deb208ed9149286d1ba90f10e3e
Akash Sahu <akkusahu559@gmail.com>
2022-11-14 22:22:15 +0530

    Using Unordered Map & Vector

--- Problems/Medium/380 - Insert Delete GetRandom O(1).cpp
+++ Problems/Medium/380 - Insert Delete GetRandom O(1).cpp
@@ -48,11 +48,3 @@ public:
         return arr[rand() % arr.size()];
     }
 };
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet* obj = new RandomizedSet();
- * bool param_1 = obj->insert(val);
- * bool param_2 = obj->remove(val);
- * int param_3 = obj->getRandom();
- */
