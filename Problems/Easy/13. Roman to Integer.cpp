1f8575cb508f2f5f60fbcf84f8721d85de1e2335
Akash Sahu <akkusahu559@gmail.com>
2023-04-28 22:25:38 +0530

    13 : [200~Left-to-Right Pass Approach

--- Problems/Easy/13. Roman to Integer.cpp
+++ Problems/Easy/13. Roman to Integer.cpp
@@ -11,7 +11,7 @@
 // Space Complexity: O(1)
 
 class Solution {
-private
+private:
     int getValue(char symbol){
         if(symbol == 'I') return 1;
         else if(symbol == 'V') return 5;
@@ -19,17 +19,19 @@ private
         else if(symbol == 'L') return 50;
         else if(symbol == 'C') return 100;
         else if(symbol == 'D') return 500;
-        else return 1000;  for M
+        else return 1000; // for M
     }
-public
+public:
     int romanToInt(string s) {
         int sum = 0, i = 0;
-        while(i  s.size()){
+        while(i < s.size()){
             char currentSymbol = s[i];
             int currentValue = getValue(currentSymbol);
             
+            // check next value
+            // if next value is big then subtract current from next value.
             int nextValue = 0;
-            if(i+1  s.size()){
+            if(i+1 < s.size()){
                 char nextSymbol = s[i+1];
                 nextValue = getValue(nextSymbol);
             }
@@ -37,7 +39,7 @@ public
             // For this case => Ex = CM 
             // M - C = 1000 - 100 => 900
             // increasing i by 2, bcz we are using 2 indexes here
-            if(currentValue  nextValue){
+            if(currentValue < nextValue){
                 sum += (nextValue - currentValue);
                 i += 2;
             }
